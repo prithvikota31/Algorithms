75d10e90041b4e7abdc2a3c0b8d426a59ac63007
prithvikota31 <prithvikota@microsoft.com>
2026-01-06 23:10:04 -0800

    Time: 37 ms (7.07%) | Memory: 50.5 MB (9.20%) - LeetSync

---
+++ 133-clone-graph/clone-graph.java
@@ -0,0 +1,69 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        //will do DFS
+        //since number of nodes not yet defined, define a set
+        //hash is better, since we need to find node and extract neighbours
+        if(node == null)    return null;
+
+        Node clone = new Node(node.val);
+        Map<Node, Node> visitedMap = new HashMap<>();
+        visitedMap.put(node, clone);
+        dfs(node, clone, visitedMap);
+        return clone;
+    }
+    //1 -> {2, 3, 4}
+    //2 -> 1, 3, 4
+    // 1 ->
+    // 2 ->
+    //hash
+    //1,1
+    //2, 2
+    //
+    
+
+    public void dfs(Node node, Node clone, Map<Node, Node> visitedMap)
+    {
+        //visited 1, 2, 3, 4
+        //        m, m
+        //node = 1, 2
+        //     
+        //     2 
+        // map
+        //1, 1 -> 2
+        //2, 2 -> 1, 3, 4
+        //3, 3
+        //4, 4
+        for(Node n: node.neighbors) //1, 3, 4
+        {
+            if(!visitedMap.containsKey(n))
+            {
+                Node cloneNeighbor = new Node(n.val);
+                visitedMap.put(n, cloneNeighbor);
+                dfs(n, cloneNeighbor, visitedMap);
+            }
+            visitedMap.get(node).neighbors.add(visitedMap.get(n));
+            System.out.println(node.val + " " + n.val);
+        }
+        
+    }
+}
