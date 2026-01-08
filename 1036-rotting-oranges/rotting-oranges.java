69ec383962b1038515877b13c7491657b5431d05
prithvikota31 <prithvikota@microsoft.com>
2026-01-07 17:47:19 -0800

    Time: 2 ms (90.14%) | Memory: 44.1 MB (58.78%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,46 +1,73 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int rows = grid.length, cols = grid[0].length;
-        Deque<Pair> q = new ArrayDeque<>();
-        int fresh = 0;
-
-        // init
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 2) q.offer(new Pair(i, j));
-                if (grid[i][j] == 1) fresh++;
+        int time = bfs(grid);
+        for(int i = 0; i < grid.length; i++)
+        {
+            for(int j = 0; j < grid[0].length; j++)
+            {
+                if(grid[i][j] == 1)
+                    return -1;
             }
         }
 
-        if (fresh == 0) return 0;
-
-        int time = 0;
-        int[] dr = {-1, 0, 1, 0};
-        int[] dc = {0, 1, 0, -1};
-
-        while (!q.isEmpty() && fresh > 0) {
-            int size = q.size();
-            time++;
-
-            for (int k = 0; k < size; k++) {
-                Pair p = q.poll();
-                for (int d = 0; d < 4; d++) {
-                    int r = p.x + dr[d];
-                    int c = p.y + dc[d];
-                    if (r >= 0 && r < rows && c >= 0 && c < cols && grid[r][c] == 1) {
-                        grid[r][c] = 2;
-                        fresh--;
-                        q.offer(new Pair(r, c));
-                    }
-                }
-            }
-        }
-
-        return fresh == 0 ? time : -1;
+        return time;
     }
+
+    public int bfs(int[][] grid)
+    {
+        Deque<Pair> q = new ArrayDeque<>();
+        int count = 0;
+        //add all rotten oranges into queue
+        for(int i = 0; i < grid.length; i++)
+        {
+            for(int j = 0; j < grid[0].length; j++)
+            {
+                if(grid[i][j] == 2)
+                    q.offer(new Pair(i, j));
+            }
+        }
+
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, 1, 0, -1};
+        if(q.isEmpty())   return 0;
+
+
+        while(!q.isEmpty())
+        {
+            int qSize = q.size();
+            count++;
+            for(int c = 0; c < qSize; c++)
+            {
+                Pair cNode = q.poll();
+                int cRow = cNode.x;
+                int cCol = cNode.y;
+
+                for(int i = 0; i < 4; i++)
+                {
+                    int nRow = cRow + delRow[i];
+                    int nCol = cCol + delCol[i];
+
+                    if(nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length &&
+                        grid[nRow][nCol] == 1)
+                    {
+                        grid[nRow][nCol] = 2;
+                        q.offer(new Pair(nRow, nCol));
+                    }
+                }           
+            }
+        }
+        return count - 1;
+    }
+
 }
 
-class Pair {
-    int x, y;
-    Pair(int x, int y) { this.x = x; this.y = y; }
+class Pair
+{
+    int x;
+    int y;
+    public Pair(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
 }
