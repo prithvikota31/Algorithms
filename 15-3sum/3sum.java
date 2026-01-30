5fa609c1678cad57c2ea48153754d4429c62e76a
prithvikota31 <prithvikota@microsoft.com>
2026-01-29 18:59:42 -0800

    Time: 33 ms (70.43%) | Memory: 59.2 MB (55.18%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,43 +1,35 @@
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
 
-        for(int i = 0; i < nums.length - 2; i++)
-        {
-            if(i > 0 && nums[i] == nums[i - 1]) // i points to first element
-            {
-                continue;
-            }
-            
+        for (int i = 0; i < nums.length - 2; i++) {
+            // skip duplicate fixed numbers
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int l = i + 1; // l second
-            int r = nums.length - 1; // r third
-            while(l < r)
-            {
-                int sum = nums[l] + nums[r] + nums[i];
-                if(sum == 0)
-                {
-                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    while(l < r && nums[l] == nums[l + 1]) l++;
-                    while(l < r && nums[r] == nums[r - 1]) r--;
+            int l = i + 1, r = nums.length - 1;
+
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+
+                    // skip duplicates for l and r
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+
                     l++;
                     r--;
-                }
-                else if(sum < 0)
-                {
-                    l++;
-                }
-                else
-                { 
-                    r--;
+                } else if (sum < 0) {
+                    l++; // need bigger sum
+                } else {
+                    r--; // need smaller sum
                 }
             }
         }
-
-        return result;
-
-        
-        
+        return res;
     }
 }
