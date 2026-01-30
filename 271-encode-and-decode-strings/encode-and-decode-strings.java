ad20f2f46a4bee6523a96cc7ce9cbfc43d1ed6c8
prithvikota31 <prithvikota@microsoft.com>
2026-01-29 17:33:03 -0800

    Time: 4 ms (97.90%) | Memory: 47.7 MB (20.93%) - LeetSync

---
+++ 271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -0,0 +1,38 @@
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+
+        for(String str: strs)
+        {
+            sb.append(str.length()).append('#').append(str);
+        }
+
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> result = new ArrayList<>();
+        //example 4#node4#soap10#xyz....
+        //2 pointers, i start, j move till #
+        int i = 0;
+        while(i < s.length())
+        {
+            int j = i;
+            while(s.charAt(j) != '#')   j++;
+            //after above while nw j is t #
+            //so length [i, j)
+            int len = Integer.parseInt(s.substring(i, j));
+            j++;
+            result.add(s.substring(j, j + len)); //eg, j = 0 and len 1, [0, 1] only 0th
+            i = j + len;
+        }
+        return result;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
