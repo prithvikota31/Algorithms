d90de6b652f2a02f0222001e775790cf72a3cf5c
prithvikota31 <prithvikota@microsoft.com>
2026-02-01 22:55:56 -0800

    Time: 70 ms (86.70%) | Memory: 85.6 MB (99.40%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+
+        List<Car> cars = new ArrayList<>();
+        for(int i = 0; i < position.length; i++)
+        {
+            cars.add(new Car());
+        }
+
+        for(int i = 0; i < position.length; i++)
+        {
+            
+            cars.get(i).position = position[i];
+            cars.get(i).time = (double)(target - position[i]) / speed[i];      
+        }
+
+        Collections.sort(cars, (a, b)->(b.position - a.position));
+
+        int count = 0;
+        double prevtime = 0;
+
+        for(int i = 0; i < cars.size(); i++)
+        {
+            if(cars.get(i).time >  prevtime)
+            {
+                count++;
+                prevtime = cars.get(i).time;
+            }
+        } 
+
+        return count;      
+    }
+}
+
+class Car {
+    int position;
+    double time;
+}
