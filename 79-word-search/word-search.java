bbd1d513e30df174c44f73954d8b1e642126b64b
prithvikota31 <prithvikota@microsoft.com>
2026-02-03 17:08:39 -0800

    Time: 187 ms (23.95%) | Memory: 43.1 MB (41.52%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,78 +1,48 @@
 class Solution {
+
+    int[] delRow = {-1, 1, 0, 0};
+    int[] delCol = {0, 0, 1, -1};
+
     public boolean exist(char[][] board, String word) {
-        //dfs and backtracking
-        //index corresponds to word pointer
+        int m = board.length, n = board[0].length;
+        boolean[][] vis = new boolean[m][n];
 
-        int[][] vis = new int[board.length][board[0].length];
-
-        if(board.length == 1 && board[0].length == 1)
-        {
-            if(word.length() == 1)
-            {
-                if(word.charAt(0) == board[0][0])
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, 0, i, j, vis))
                     return true;
             }
+        }
+        return false;
+    }
 
+    private boolean dfs(char[][] board, String word, int idx,
+                        int row, int col, boolean[][] vis) {
+
+        // bounds + visited + mismatch
+        if (row < 0 || col < 0 ||
+            row >= board.length || col >= board[0].length ||
+            vis[row][col] ||
+            board[row][col] != word.charAt(idx))
             return false;
-        }
-    
 
-        int[] delRow = {-1, 1, 0, 0};
-        int[] delCol = {0, 0, 1, -1};
+        // last character matched
+        if (idx == word.length() - 1)
+            return true;
 
-        //dfs path 4 directions(-1, 0)
-        //(1, 0)
-        //(0, 1)
-        //(0 , -1)
-        for(int i = 0; i < board.length; i++)
-        {
-            for(int j = 0; j < board[0].length; j++)
-            {
-                if(word.charAt(0) == board[i][j])
-                    {if(dfs(board, word, 0, vis, i, j, delRow, delCol))
-                        return true;}               
-            }
-           
-        }
-        return false;
+        vis[row][col] = true;
 
-    }
+        for (int d = 0; d < 4; d++) {
+            int nr = row + delRow[d];
+            int nc = col + delCol[d];
 
-public boolean dfs(char[][] board, String word, int index, int[][] vis, int row, int col, int[] delRow, int[] delCol)
-{
-    
-    if(index == word.length())
-        return true;
-
-    if(row >=0 && row < board.length && col >=0 && col < board[0].length && vis[row][col] == 0)
-    {
-        vis[row][col] = 1;
-    }
-    else
-    {
-        return false;
-    }
-    
-
-
-
-
-    if(word.charAt(index) == board[row][col])
-    {
-        for(int i = 0; i < 4; i++)
-        {
-            int curRow = row + delRow[i];
-            int curCol = col + delCol[i];
-
-            if(dfs(board, word, index + 1, vis, curRow, curCol, delRow, delCol))
-            {
-                vis[row][col] = 0;
+            if (dfs(board, word, idx + 1, nr, nc, vis)) {
+                vis[row][col] = false;
                 return true;
             }
-        }                 
-    }
-    vis[row][col] = 0;
-    return false;
+        }
 
-}
+        vis[row][col] = false;
+        return false;
+    }
 }
