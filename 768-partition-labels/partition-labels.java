7d96fc265a6b2fee2b4ef44c7a89d277de21e448
prithvikota31 <prithvikota@microsoft.com>
2026-02-03 19:13:26 -0800

    Time: 5 ms (81.08%) | Memory: 43.6 MB (67.38%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -1,31 +1,20 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        List<Integer> result = new ArrayList<>();
-        int n = s.length();
-        int start = 0;
-
-        while (start < n) {
-            int end = start;
-
-            // Check how far this partition needs to go
-            for (int i = start; i <= end; i++) {
-                char currentChar = s.charAt(i);
-                // Scan the entire string to find the last occurrence of currentChar
-                for (int j = n - 1; j > end; j--) {
-                    if (s.charAt(j) == currentChar) {
-                        end = j;
-                        break;
-                    }
-                }
-            }
-
-            // Once we find the end of the partition, add it
-            result.add(end - start + 1);
-            start = end + 1;
+        int[] last = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
         }
 
-        return result;
+        List<Integer> res = new ArrayList<>();
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+            if (i == end) {
+                res.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+        return res;
     }
 }
