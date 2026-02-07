58de608ad35a6c203b58387d1f33af54d35bd5a4
prithvikota31 <prithvikota@microsoft.com>
2026-02-06 17:00:17 -0800

    Time: 0 ms (100.00%) | Memory: 44.2 MB (86.40%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,41 +10,44 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
-        if(list1 == null)   return list2;
-        if(list2 == null)   return list1;
-        //maintain list1 with small value
-        if(list1.val > list2.val)   return mergeTwoLists(list2, list1);
-        ListNode head = list1;
-
-        //  1 2 5 4 
-        //  2 3 4
-        while(list1 != null && list2 != null)
+        if(list1 == null)
         {
-            if(list1.next == null)
-            {
-                list1.next = list2;
-                break;
-            }
-
-            if(list1.next.val < list2.val)
-            {
-                list1 = list1.next;
-            }
-
-            else
-            {
-                ListNode temp = list2.next;
-                list2.next = list1.next;
-                list1.next = list2;
-                list2 = temp;
-                list1 = list1.next;
-            }
-
-
+            return list2;
         }
 
+        if(list2 == null)
+        {
+            return list1;
+        }
+     
+        ListNode head = new ListNode(-1);
+        ListNode curr = head;
 
-        return head;
+        while(list1 != null && list2 != null)
+        {
+            if(list1.val < list2.val) //&& list1 != null)
+            {
+                curr.next = list1;
+                list1 = list1.next;
+
+            }
+            else
+            {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if(list1 != null) {
+            curr.next = list1;
+        }
+        else
+        {
+            curr.next = list2;
+        }
+
+        return head.next;
+        
     }
 }
