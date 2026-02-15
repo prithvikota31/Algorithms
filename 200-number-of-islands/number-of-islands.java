68d1caf139837cb7a852146245960417f33c3361
prithvikota31 <prithvikota@microsoft.com>
2026-02-15 12:19:10 -0800

    Time: 4 ms (48.86%) | Memory: 52.5 MB (45.82%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,68 +1,42 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        //each cell is a node
-        //go with bfs
         int m = grid.length;
         int n = grid[0].length;
-        int[][] visited = new int[m][n];
-
-        int[] delRow = {0, 1, 0, -1};
-        int[] delCol = {1, 0, -1, 0};
-
+        boolean[][] visited = new boolean[m][n];
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, 1, 0, -1};
         int count = 0;
+
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
             {
-                if(visited[i][j] == 0 && grid[i][j] == '1')
+                if(!visited[i][j] && grid[i][j] == '1')
                 {
-                    bfs(grid, visited, delRow, delCol, i , j);
+                    dfs(grid, visited, i, j, delRow, delCol);
                     count++;
                 }
             }
         }
+
         return count;
     }
 
-    public void bfs(char[][] grid, int[][] visited, int[] delRow, int[] delCol, int i, int j)
+
+    public void dfs(char[][] grid, boolean[][] visited , int r, int c, int[] delRow, int[] delCol)
     {
-        Queue<Pair> q = new LinkedList<>();
-        visited[i][j] = 1;
-        q.add(new Pair(i, j));
-        int m = visited.length;
-        int n = visited[0].length;
+        visited[r][c] = true;
 
-        while(!q.isEmpty())
+        for(int i = 0; i < 4; i++)
         {
-            Pair cur = q.poll();
-            int curX = cur.x;
-            int curY = cur.y;
+            int nRow = delRow[i] + r;
+            int nCol = delCol[i] + c;
 
-            for(int it = 0; it < 4; it++)
+            if(nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length &&
+                !visited[nRow][nCol] && grid[nRow][nCol] == '1')
             {
-                int neighX = curX + delRow[it];
-                int neighY = curY + delCol[it];
-
-                if(neighX >= 0 && neighX <= m - 1 && neighY >= 0 && neighY <= n - 1 
-                    && visited[neighX][neighY] == 0 && grid[neighX][neighY] == '1')
-                {
-                    visited[neighX][neighY] = 1;
-                    q.add(new Pair(neighX, neighY));
-                }
+                dfs(grid, visited, nRow, nCol, delRow, delCol);
             }
         }
-    }
-
-}
-
-
-class Pair{
-    int x;
-    int y;
-
-    public Pair(int x, int y)
-    {
-        this.x = x;
-        this.y = y;
     }
 }
