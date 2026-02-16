a6cd5df5013ecba411ebfc16e7515b82dcf175d1
prithvikota31 <prithvikota@microsoft.com>
2026-02-16 00:57:24 -0800

    Time: 19 ms (5.37%) | Memory: 48.1 MB (16.58%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) 
+    {
+        // the problem is to find a topological sort if it exists or not
+        //first create a graph
+        List<List<Integer>> graph = new LinkedList<>();
+        int v = numCourses;
+        //create an empty graph
+        for(int i = 0; i < numCourses; i++)
+        {
+            graph.add(new LinkedList<>());
+        }
+
+        for(int i = 0; i < prerequisites.length; i++)
+        {
+            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        // till now graph is created
+        int[] inDegree = new int[v];
+        for(int i = 0; i < v; i++)
+        {
+            for(int it: graph.get(i))
+            {
+                inDegree[it]++;
+            }
+        }
+
+        return topoSort(graph, v, inDegree);
+    }
+
+
+    public int[] topoSort(List<List<Integer>> graph, int v, int[] inDegree)
+    {
+        Queue<Integer> q = new LinkedList<>();
+
+        //add all nodes with 0 indegree
+        for(int i = 0; i < inDegree.length; i++)
+        {
+            if(inDegree[i] == 0)
+                q.add(i);
+        }
+        int[] topoSort = new int[v];
+        int ind = 0;
+
+        while(!q.isEmpty())
+        {
+            int curNode = q.poll();
+            topoSort[ind++] = curNode;
+
+            for(Integer it : graph.get(curNode))
+            {
+                inDegree[it]--;
+                if(inDegree[it] == 0)
+                {
+                    q.add(it);
+                }
+            }
+        }
+
+        if(ind == v)    return topoSort;
+        else return new int[0];
+    }
+}
