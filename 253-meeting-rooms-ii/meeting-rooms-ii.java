d147281a3470adb1d41381a14d7ba4e64cd6d61f
prithvikota31 <prithvikota@microsoft.com>
2026-02-23 21:19:35 -0800

    Time: 8 ms (94.60%) | Memory: 48.9 MB (5.69%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,41 +1,38 @@
+import java.util.Arrays;
 
-import java.lang.reflect.Array;class Solution {
+class Solution {
     public int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
+
         int n = intervals.length;
 
         int[] start = new int[n];
         int[] end = new int[n];
 
-        for(int i = 0; i < n; i++)
-        {
+        // Separate start and end times
+        for (int i = 0; i < n; i++) {
             start[i] = intervals[i][0];
             end[i] = intervals[i][1];
         }
+
         Arrays.sort(start);
         Arrays.sort(end);
 
-        int i = 0; 
-        int j = 0;
+        int rooms = 0;
+        int endPtr = 0;
 
-        int cur = 0;
-        int res = 0;
+        for (int i = 0; i < n; i++) {
 
-        while(i < n)
-        {
-            if(start[i] < end[j])
-            {
-                cur++;
-                i++;
+            // If next meeting starts before earliest meeting ends
+            if (start[i] < end[endPtr]) {
+                rooms++;          // need new room
+            } else {
+                endPtr++;         // reuse a freed room
             }
-            else
-            {
-                cur--;
-                j++;
-            }
-
-            res = Math.max(cur, res);
         }
 
-        return res;
+        return rooms;
     }
 }
