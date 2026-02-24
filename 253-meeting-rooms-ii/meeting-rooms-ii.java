f603e22a814f84bf653b75f120fef6f601e3c09b
prithvikota31 <prithvikota@microsoft.com>
2026-02-23 21:00:11 -0800

    Time: 9 ms (50.46%) | Memory: 49 MB (5.69%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,41 @@
+
+import java.lang.reflect.Array;class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int n = intervals.length;
+
+        int[] start = new int[n];
+        int[] end = new int[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        int i = 0; 
+        int j = 0;
+
+        int cur = 0;
+        int res = 0;
+
+        while(i < n && j < n)
+        {
+            if(start[i] < end[j])
+            {
+                cur++;
+                i++;
+            }
+            else
+            {
+                cur--;
+                j++;
+            }
+
+            res = Math.max(cur, res);
+        }
+
+        return res;
+    }
+}
