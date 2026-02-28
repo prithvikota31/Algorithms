aea3c0bbe87cf8b50d185716dedde7cc54aca92f
prithvikota31 <prithvikota@microsoft.com>
2026-02-28 15:56:15 -0800

    Time: 6 ms (72.03%) | Memory: 45.1 MB (46.48%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,31 +1,42 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
 
+        int n = nums.length;
+
+        // Step 1: calculate total sum
         int totalSum = 0;
         for (int num : nums) {
             totalSum += num;
         }
 
-        // Invalid cases: cannot partition
+        // Step 2: check invalid cases
         if ((totalSum + target) % 2 != 0 || Math.abs(target) > totalSum) {
             return 0;
         }
 
         int newTarget = (totalSum + target) / 2;
 
-        // dp[j] = number of ways to get sum j
-        int[] dp = new int[newTarget + 1];
+        // Step 3: create dp table
+        // dp[i][j] = number of ways to form sum j using first i elements
+        int[][] dp = new int[n + 1][newTarget + 1];
 
-        // Base case: one way to make sum 0
-        dp[0] = 1;
+        // Step 4: base case
+        dp[0][0] = 1;
 
-        for (int num : nums) {
-            // Traverse backwards to avoid reuse in same iteration
-            for (int j = newTarget; j >= num; j--) {
-                dp[j] += dp[j - num];
+        // Step 5: fill table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= newTarget; j++) {
+
+                // exclude current element
+                dp[i][j] = dp[i - 1][j];
+
+                // include current element
+                if (nums[i - 1] <= j) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                }
             }
         }
 
-        return dp[newTarget];
+        return dp[n][newTarget];
     }
 }
