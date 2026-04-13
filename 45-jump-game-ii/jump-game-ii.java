69079ad6bef10dcec790ab7e9ea114c8ed2cef66
prithvikota31 <prithvikota@microsoft.com>
2026-04-13 01:09:07 -0700

    Time: 1 ms (99.60%) | Memory: 47.4 MB (25.51%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,28 +1,24 @@
 class Solution {
     public int jump(int[] nums) {
-        // dp is a way, but grredy is better
-        //2,3,1,1,4
-        //0 1 2 3 4
-        //..l...r
-        // Maintain range after each jump;
-        // by default l = 0, r = 0 for jump = 0;
-        // 
+        int n = nums.length;
+        int count = 0;
+        int maxJump = 0;
+        int j = 0;
 
-
-        int l = 0, r = 0, jump = 0;
-
-        while(r < nums.length - 1) //if r is already on n - 1 extra jump not needed
+        for(int i = 0; i < n; i++)
         {
-            int nextJump = r + 1; //(minimum I can think of)
-            for(int i = l; i <= r; i++)
-            {
-                nextJump = Math.max(nextJump, i + nums[i]);
-            }
-            l = r + 1;
-            r = nextJump;
-            jump++;
-        }
-        return jump;
+            if(j >= n - 1)  return count;
 
+            if(i > j)
+            {
+                count++;
+                j = maxJump;
+            }
+
+            maxJump = Math.max(maxJump, i + nums[i]);
+        }
+
+
+        return count;
     }
 }
