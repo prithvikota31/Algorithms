d4cfcff9b46a78df3fc8296c15869b558a7ae1d1
prithvikota31 <prithvikota@microsoft.com>
2026-04-13 19:45:30 -0700

    Time: 59 ms (41.97%) | Memory: 47.9 MB (63.68%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -11,40 +11,52 @@ class Solution {
         }
 
 
-        while(!sortedMap.isEmpty())
-        {
-            int cur = sortedMap.firstKey();
-            for(int i = 0; i < groupSize - 1; i++)
-            {
-                int curValue = sortedMap.get(cur);
-                if(curValue == 1)
-                {
-                   sortedMap.remove(cur); 
-                }
-                else
-                {
-                    sortedMap.put(cur, curValue - 1);
-                }
+        // while(!sortedMap.isEmpty())
+        // {
+        //     int cur = sortedMap.firstKey();
+        //     for(int i = 0; i < groupSize - 1; i++)
+        //     {
+        //         int curValue = sortedMap.get(cur);
+        //         if(curValue == 1)
+        //         {
+        //            sortedMap.remove(cur); 
+        //         }
+        //         else
+        //         {
+        //             sortedMap.put(cur, curValue - 1);
+        //         }
 
-                if(sortedMap.containsKey(cur + 1))
-                {
-                    cur += 1;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            if(sortedMap.get(cur) == 1)
-            {
-                sortedMap.remove(cur); 
-            }
-            else
-            {
-                sortedMap.put(cur, sortedMap.get(cur) - 1);
-            }
+        //         if(sortedMap.containsKey(cur + 1))
+        //         {
+        //             cur += 1;
+        //         }
+        //         else
+        //         {
+        //             return false;
+        //         }
+        //     }
+        //     if(sortedMap.get(cur) == 1)
+        //     {
+        //         sortedMap.remove(cur); 
+        //     }
+        //     else
+        //     {
+        //         sortedMap.put(cur, sortedMap.get(cur) - 1);
+        //     }
+        while (!sortedMap.isEmpty()) {
+            int start = sortedMap.firstKey();
 
+            for (int i = 0; i < groupSize; i++) {
+                int curr = start + i;
+
+                if (!sortedMap.containsKey(curr)) return false;
+
+                int count = sortedMap.get(curr);
+                if (count == 1) sortedMap.remove(curr);
+                else sortedMap.put(curr, count - 1);
+            }
         }
+
 
         return true;
     }
