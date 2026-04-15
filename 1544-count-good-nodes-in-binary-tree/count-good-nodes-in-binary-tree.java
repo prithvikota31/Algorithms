1af12d85a95716649b2eed6543fe5fe35dfadd5c
prithvikota31 <prithvikota@microsoft.com>
2026-04-14 22:29:26 -0700

    Time: 2 ms (94.41%) | Memory: 57.1 MB (53.18%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -1,19 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public int goodNodes(TreeNode root) {
-        return dfs(root, root.val);
+        int[] result = new int[1];
+        result[0]++;
+        dfs(root, result, root.val);
+        return result[0];
     }
 
-    private int dfs(TreeNode node, int maxSoFar) {
-        if (node == null) return 0;
+    private void dfs(TreeNode root, int[] result, int maxSoFar)
+    {
+        if(root == null)    return;
 
-        int count = 0;
-        if (node.val >= maxSoFar) count = 1;
+        if(root.left != null)
+        {
+            if(root.left.val >=  maxSoFar)
+            {
+                result[0]++;
+                dfs(root.left, result, root.left.val);
+            }
+            else
+            {
+                dfs(root.left, result, maxSoFar);
+            }    
+        }
 
-        maxSoFar = Math.max(maxSoFar, node.val);
+        if(root.right != null)
+        {
+            if(root.right.val >=  maxSoFar)
+            {
+                result[0]++;
+                dfs(root.right, result, root.right.val);
+            }
+            else
+            {
+                dfs(root.right, result, maxSoFar);
+            }    
+        }
 
-        count += dfs(node.left, maxSoFar);
-        count += dfs(node.right, maxSoFar);
 
-        return count;
     }
 }
