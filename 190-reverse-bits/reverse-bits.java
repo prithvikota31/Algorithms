3608a467a8c44f722f1e46ddd0d694ac69ca8bdc
prithvikota31 <prithvikota@microsoft.com>
2026-05-05 15:19:31 -0700

    Time: 1 ms (60.28%) | Memory: 42.2 MB (83.60%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i = 0; i < 32; i++)
         {
             int lastbit = n & 1;
-            n = n >> 1;
+            n = n >>> 1;
             ans = ans << 1;  
             ans = ans | lastbit;
             
