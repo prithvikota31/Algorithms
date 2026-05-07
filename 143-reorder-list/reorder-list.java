6b9b2897fdd0fac917c1afbfdb902e7194c006ad
prithvikota31 <prithvikota@microsoft.com>
2026-05-06 23:50:48 -0700

    Time: 2 ms (88.67%) | Memory: 48.6 MB (98.86%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,64 +1,68 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+
     public void reorderList(ListNode head) {
-        ListNode ans = head;
+
+        if(head == null || head.next == null) {
+            return;
+        }
+
         ListNode middle = middleOfLL(head);
+
         ListNode head2 = reverseLL(middle.next);
         middle.next = null;
+
         ListNode head1 = head;
 
-        while(head1 != null && head2 != null)
-        {
-            ListNode t1 = head1.next;
-            ListNode t2 = head2.next;
+        // DUMMY NODE
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
 
-            head1.next = head2;
-            head2.next = t1;
+        while(head1 != null && head2 != null) {
 
-            head1 = t1;
-            head2 = t2;
+            // attach from first half
+            tail.next = head1;
+            head1 = head1.next;
+            tail = tail.next;
 
-        } 
-        
+            // attach from second half
+            tail.next = head2;
+            head2 = head2.next;
+            tail = tail.next;
+        }
+
+        // leftover node (odd length case)
+        if(head1 != null) {
+            tail.next = head1;
+        }
     }
 
-    public ListNode middleOfLL(ListNode head)
-    {
+    public ListNode middleOfLL(ListNode head) {
+
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null)
-        {
+        while(fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
         return slow;
-
     }
 
-     public ListNode reverseLL(ListNode head)
-    {
+    public ListNode reverseLL(ListNode head) {
+
         ListNode prev = null;
         ListNode curr = head;
-        while(curr != null)
-        {
+
+        while(curr != null) {
+
             ListNode next = curr.next;
+
             curr.next = prev;
             prev = curr;
             curr = next;
         }
 
         return prev;
-
     }
 }
