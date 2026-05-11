ea9d71fb05319fc39e89c8e554fca4b1f65540b9
prithvikota31 <prithvikota@microsoft.com>
2026-05-11 01:53:07 -0700

    Time: 4 ms (98.70%) | Memory: 47.5 MB (33.03%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.java
+++ 271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -2,33 +2,38 @@ public class Codec {
 
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        StringBuilder sb = new StringBuilder();
+        //length, delimiter, str
 
+        StringBuilder sb = new StringBuilder();
         for(String str: strs)
         {
-            sb.append(str.length()).append('#').append(str);
+            sb.append(str.length()).append('*').append(str);
         }
-
         return sb.toString();
     }
 
     // Decodes a single string to a list of strings.
+    //eg: 3*abc5*daceb
     public List<String> decode(String s) {
-        List<String> result = new ArrayList<>();
-        //example 4#node4#soap10#xyz....
-        //2 pointers, i start, j move till #
+        
         int i = 0;
+        List<String> result = new ArrayList<>();
         while(i < s.length())
         {
+            //need j pointer to capture number
             int j = i;
-            while(s.charAt(j) != '#')   j++;
-            //after above while now j is at #
-            //so length [i, j) exclude j
+            while(s.charAt(j) != '*')
+            {
+                j++;
+            }
+            //after while j is at *
             int len = Integer.parseInt(s.substring(i, j));
             j++;
-            result.add(s.substring(j, j + len)); //eg, j = 0 and len 1, [0, 1] only 0th
+            result.add(s.substring(j, j + len));
             i = j + len;
+
         }
+
         return result;
     }
 }
