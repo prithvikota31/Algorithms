f5f9bfdfbdb8dad1d7238be2f95b9d4de52da8ca
prithvikota31 <prithvikota@microsoft.com>
2026-05-11 00:24:47 -0700

    Time: 2 ms (95.24%) | Memory: 71.8 MB (61.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -2,30 +2,33 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         
         int n = nums.length;
-        int[] rightProduct = new int[n];
+        int[] ans = new int[n];
 
-        rightProduct[n - 1] = 1; //since nothing on right of last
+        ans[n - 1] = 1; //first capture right product
 
         for(int i = n - 2; i >= 0; i--)
         {
-            rightProduct[i] = nums[i + 1] * rightProduct[i + 1];
+            ans[i] = nums[i + 1] * ans[i + 1];
         } 
 
-        int[] leftProduct = new int[n];
+        // int[] leftProduct = new int[n];
 
-        leftProduct[0] = 1;
+        // leftProduct[0] = 1;
 
+        int leftProduct = 1;
+        //now capture left product
         for(int i = 1; i <= n -1; i++)
         {
-            leftProduct[i] = leftProduct[i - 1] * nums[i - 1]; 
+            ans[i] *= leftProduct * nums[i - 1]; 
+            leftProduct *= nums[i - 1];
         }
 
-        int[] ans = new int[n];
+        // int[] ans = new int[n];
 
-        for(int i = 0; i < n; i++)
-        {
-            ans[i] = leftProduct[i] * rightProduct[i];
-        }
+        // for(int i = 0; i < n; i++)
+        // {
+        //     ans[i] = leftProduct[i] * rightProduct[i];
+        // }
 
         return ans;
 
