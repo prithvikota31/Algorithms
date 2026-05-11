f054fee33513d672aec2cf7c876f84e68728be2c
prithvikota31 <prithvikota@microsoft.com>
2026-05-11 02:11:33 -0700

    Time: 65 ms (28.33%) | Memory: 48.1 MB (48.12%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,82 +1,34 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        int n = hand.length;
-        if(n % groupSize != 0)  return false;
-
-        for(int i = 0; i < n; i++)
-        {
-            map.put(hand[i], map.getOrDefault(hand[i], 0) + 1);
-        }
-
-        Arrays.sort(hand);
+        if(hand.length % groupSize != 0)    return false;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
 
         for(int num: hand)
         {
-            int curFreq= map.get(num);
-            if(curFreq == 0)  continue;
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
 
+
+        while(!map.isEmpty())
+        {
+            
+            int card = map.firstKey();
             for(int i = 0; i < groupSize; i++)
             {
-                int cur = num + i;
-                if(map.getOrDefault(cur, 0) == 0)  return false;
-
-                map.put(cur, map.get(cur) - 1);
+                if(!map.containsKey(card))
+                {
+                    return false;
+                }
+                map.put(card, map.get(card) - 1);
+                if(map.get(card) == 0)
+                {
+                    map.remove(card);
+                }
+                card++;
             }
-
         }
 
         return true;
 
-
-        // while(!sortedMap.isEmpty())
-        // {
-        //     int cur = sortedMap.firstKey();
-        //     for(int i = 0; i < groupSize - 1; i++)
-        //     {
-        //         int curValue = sortedMap.get(cur);
-        //         if(curValue == 1)
-        //         {
-        //            sortedMap.remove(cur); 
-        //         }
-        //         else
-        //         {
-        //             sortedMap.put(cur, curValue - 1);
-        //         }
-
-        //         if(sortedMap.containsKey(cur + 1))
-        //         {
-        //             cur += 1;
-        //         }
-        //         else
-        //         {
-        //             return false;
-        //         }
-        //     }
-        //     if(sortedMap.get(cur) == 1)
-        //     {
-        //         sortedMap.remove(cur); 
-        //     }
-        //     else
-        //     {
-        //         sortedMap.put(cur, sortedMap.get(cur) - 1);
-        //     }
-        // while (!sortedMap.isEmpty()) {
-        //     int start = sortedMap.firstKey();
-
-        //     for (int i = 0; i < groupSize; i++) {
-        //         int curr = start + i;
-
-        //         if (!sortedMap.containsKey(curr)) return false;
-
-        //         int count = sortedMap.get(curr);
-        //         if (count == 1) sortedMap.remove(curr);
-        //         else sortedMap.put(curr, count - 1);
-        //     }
-        // }
-
-
-        // return true;
     }
 }
