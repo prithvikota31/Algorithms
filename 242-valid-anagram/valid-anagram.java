edcf0e478779902fb218f41150037e16e576f471
prithvikota31 <prithvikota@microsoft.com>
2026-05-10 21:41:32 -0700

    Time: 5 ms (62.04%) | Memory: 44 MB (94.63%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,21 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())
-        {
+
+        // lengths must match
+        if(s.length() != t.length()) {
             return false;
         }
 
         int[] freq = new int[26];
-        for(int i = 0; i < s.length(); i++)
-        {
+
+        // count characters
+        for(int i = 0; i < s.length(); i++) {
+
             freq[s.charAt(i) - 'a']++;
+
+            freq[t.charAt(i) - 'a']--;
         }
 
-        for(int i = 0; i < t.length(); i++)
-        {
-            freq[t.charAt(i) - 'a']--;
-            if(freq[t.charAt(i) - 'a'] < 0)
-            {
+        // verify all balanced
+        for(int count : freq) {
+            if(count != 0) {
                 return false;
             }
         }
