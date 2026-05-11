0bb991eb013db6f13d62c69b3ead2622343af7c8
prithvikota31 <prithvikota@microsoft.com>
2026-05-11 01:58:50 -0700

    Time: 2 ms (99.35%) | Memory: 119.6 MB (24.81%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,30 +1,26 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        //at any station net gas to go to next station
-        //availableGas + gas[i] - cost[i] to go next station
-        // if above value is positive it can go, otherwise break
-
-        //check net gas +ve or not, otherwise it is not possible
-        //calculate this from 0th index to last, at any time, net becomes -ve, we can skip gasStations till that point
-
-
         int availableGas = 0;
-        int net = 0;
-        int startIndex = 0;
+        int netGas = 0;
+
         for(int i = 0; i < gas.length; i++)
         {
-            availableGas = availableGas + gas[i] - cost[i];
+            netGas += gas[i] - cost[i];
+        }
+        if(netGas < 0)  return -1;
 
-            net += (gas[i] - cost[i]);
-
+        int start = 0;
+        for(int i = 0; i < gas.length; i++)
+        {
+            availableGas += gas[i] - cost[i];
             if(availableGas < 0)
             {
                 availableGas = 0;
-                startIndex = i + 1;
+                start = i + 1;
             }
         }
 
-        return net >= 0? startIndex: -1;
 
+        return start;
     }
 }
