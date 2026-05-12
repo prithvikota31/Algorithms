dc669161c42eb246974522498d26659dd15a0077
prithvikota31 <prithvikota@microsoft.com>
2026-05-11 22:55:24 -0700

    Time: 32 ms (17.46%) | Memory: 46.7 MB (35.09%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,32 +1,39 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        //dp[i][j] => till ith index of coins, minimum coins to make amount j;
 
-        int[] dp = new int[amount + 1];
+        int[][] dp = new int[coins.length][amount + 1];
 
-        // Fill with impossible large value
-        Arrays.fill(dp, amount + 1);
+        for(int i = 0; i < coins.length; i++)
+        {
+            Arrays.fill(dp[i], (int)1e9);
+        }
 
-        // Base case
-        dp[0] = 0;
+        //base condition if i = 0, only 1st coin, check if remainder is zero
 
-        // Build answers from 1 -> amount
-        for (int i = 1; i <= amount; i++) {
-
-            // Try every coin
-            for (int coin : coins) {
-
-                // Coin can participate
-                if (coin <= i) {
-
-                    dp[i] = Math.min(
-                        dp[i],
-                        1 + dp[i - coin]
-                    );
-                }
+        for(int j = 0; j <= amount; j++)
+        {
+            if(j % coins[0] == 0)
+            {
+                dp[0][j] = j / coins[0];
             }
         }
 
-        // Impossible case
-        return dp[amount] > amount ? -1 : dp[amount];
+        for(int i = 1; i < coins.length; i++)
+        {
+            for(int j = 0; j <= amount; j++)
+            {
+                int take = (int)1e9;
+                if(coins[i] <= j)
+                {
+                    take = 1 + dp[i][j - coins[i]];
+                }
+                int notTake = dp[i - 1][j];
+                dp[i][j] = Math.min(take, notTake);
+            }
+        }
+
+        if(dp[coins.length - 1][amount] >= (int)1e9)    return -1;
+        return dp[coins.length - 1][amount];
     }
 }
