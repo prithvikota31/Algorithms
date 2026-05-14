d536632c065084eaf61a8f5d01cf90f4bfbd5248
prithvikota31 <prithvikota@microsoft.com>
2026-05-14 03:33:23 -0700

    Time: 103 ms (75.08%) | Memory: 135.2 MB (97.02%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.java
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.java
@@ -1,65 +1,75 @@
 class Solution {
     public int[] minInterval(int[][] intervals, int[] queries) {
-
         int n = queries.length;
 
-        // answer array
-        int[] result = new int[n];
+        //sort intervals by start
+        //sort queries based on values but store the index
+        Arrays.sort(intervals, (a, b)-> (a[0] - b[0]));
 
-        // queries with original index
-        int[][] q = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            q[i][0] = queries[i];
-            q[i][1] = i;
+        List<QueryPair> queryPairIndexList = new ArrayList<>();
+        for(int i = 0; i < queries.length; i++)
+        {
+            queryPairIndexList.add(new QueryPair(i, queries[i]));
         }
 
-        // sort intervals by start
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        Collections.sort(queryPairIndexList, (a, b) -> (a.value - b.value));
+        int[] result = new int[n];
+        //size = end - start + 1;
+        PriorityQueue<IntervalPair> pq = new PriorityQueue<>((a, b) -> (a.size - b.size));
 
-        // sort queries by value
-        Arrays.sort(q, (a, b) -> a[0] - b[0]);
+        int ind = 0;
+        //for each query sweep from left to right
+        for(int i = 0; i < queryPairIndexList.size(); i++)
+        {
+            QueryPair curQuery = queryPairIndexList.get(i);
 
-        // minHeap -> [intervalSize, intervalEnd]
-        PriorityQueue<int[]> minHeap =
-            new PriorityQueue<>((a, b) -> a[0] - b[0]);
-
-        int i = 0;
-
-        for (int[] query : q) {
-
-            int queryValue = query[0];
-            int originalIndex = query[1];
-
-            // add all intervals whose start <= query
-            while (i < intervals.length &&
-                   intervals[i][0] <= queryValue) {
-
-                int start = intervals[i][0];
-                int end = intervals[i][1];
-
-                int size = end - start + 1;
-
-                minHeap.offer(new int[]{size, end});
-
-                i++;
+            while(ind < intervals.length && intervals[ind][0] <= curQuery.value)
+            {
+                int tSize = intervals[ind][1] - intervals[ind][0] + 1;
+                pq.offer(new IntervalPair(intervals[ind][1], tSize));
+                ind++;
             }
 
-            // remove intervals that cannot contain query
-            while (!minHeap.isEmpty() &&
-                   minHeap.peek()[1] < queryValue) {
-
-                minHeap.poll();
+            while(!pq.isEmpty() && pq.peek().end < curQuery.value)
+            {
+                pq.poll();
             }
 
-            // heap top = smallest valid interval
-            if (minHeap.isEmpty()) {
-                result[originalIndex] = -1;
-            } else {
-                result[originalIndex] = minHeap.peek()[0];
+            if(pq.isEmpty())
+            {
+                result[curQuery.index] = -1;
             }
+            else
+            {
+                result[curQuery.index] = pq.peek().size;
+            }
+
         }
 
         return result;
+        
+    }
+
+
+}
+
+class IntervalPair{
+
+    int end;
+    int size;
+    public IntervalPair(int end, int size)
+    {
+        this.end = end;
+        this.size = size;
+    }
+}
+
+class QueryPair{
+    int index;
+    int value;
+    public QueryPair(int ind, int val)
+    {
+        index = ind;
+        value = val;
     }
 }
