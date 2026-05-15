014ef3ec1165f4971d78ec1f22bf30c36b83cf12
prithvikota31 <prithvikota@microsoft.com>
2026-05-14 20:27:55 -0700

    Time: 5 ms (97.85%) | Memory: 43.9 MB (85.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,52 +1,44 @@
-
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
 
-        int[] freq = new int[26];
-
-        // Step 1: build frequency map for s1
-        for (char c : s1.toCharArray()) {
-            freq[c - 'a']++;
+        // Impossible case
+        if (s1.length() > s2.length()) {
+            return false;
         }
 
-        int left = 0;
-        int right = 0;
-        int needed = s1.length();
+        int[] s1Freq = new int[26];
+        int[] windowFreq = new int[26];
 
-        // Step 2: sliding window over s2
-        while (right < s2.length()) {
-            char rc = s2.charAt(right);
+        // Build frequency for s1
+        for (char c : s1.toCharArray()) {
+            s1Freq[c - 'a']++;
+        }
 
-            if (freq[rc - 'a'] > 0) {
-                needed--;
+        int windowSize = s1.length();
+
+        // Build first window
+        for (int i = 0; i < windowSize; i++) {
+            windowFreq[s2.charAt(i) - 'a']++;
+        }
+
+        // Check first window
+        if (Arrays.equals(s1Freq, windowFreq)) {
+            return true;
+        }
+
+        // Slide the window
+        for (int right = windowSize; right < s2.length(); right++) {
+
+            // Add new character
+            windowFreq[s2.charAt(right) - 'a']++;
+
+            // Remove left character
+            windowFreq[s2.charAt(right - windowSize) - 'a']--;
+
+            // Compare frequencies
+            if (Arrays.equals(s1Freq, windowFreq)) {
+                return true;
             }
-            freq[rc - 'a']--;
-            //freq if its -3 we have 3 extra char in the fixed window
-            //freq = 3 we still need 3 char to have our premutation
-            
-
-            // Step 3: if window size > s1, shrink from left
-            if (right - left >= s1.length()) {
-                char lc = s2.charAt(left);
-
-                //why >= 0
-                //right has already traversed this
-                //if the letter doesn't exist at all, it would have become -ve when right traversed
-                //which implies, if its freq is zero or >0, since it moves out of the window increased the needed
-                //also, this implies when that freq is incrmented it goes to positive which means needed increases
-
-                freq[lc - 'a']++;
-                if (freq[lc - 'a'] > 0)  
-                { 
-                    needed++; //track the needed when moving left and right
-                }
-                left++;
-            }
-
-            // Step 4: check match
-            if (needed == 0) return true;
-            right++;
         }
 
         return false;
