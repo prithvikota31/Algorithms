4c346e21aed99a2d3ec8e9e4ca456be19844fbeb
prithvikota31 <prithvikota@microsoft.com>
2026-05-14 23:10:43 -0700

    Time: 3 ms (95.82%) | Memory: 46.2 MB (54.87%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,41 +1,52 @@
 class Solution {
+
     public int lengthOfLIS(int[] nums) {
-        int[] target = new int[nums.length];
-        target[0] = nums[0];
-        int ind = 0;
-        for(int i = 1; i < nums.length; i++)
-        {
-            if(nums[i] > target[ind])
-            {
-                target[++ind] = nums[i];
+
+        int[] tails = new int[nums.length];
+
+        tails[0] = nums[0];
+
+        int lisEndIndex = 0;
+
+        for(int i = 1; i < nums.length; i++) {
+
+            // Current number can extend LIS
+            if(nums[i] > tails[lisEndIndex]) {
+
+                tails[++lisEndIndex] = nums[i];
                 continue;
             }
-            int replaceIndex = firstNoGreater(target, 0, ind, nums[i]);
-            target[replaceIndex] = nums[i];
+
+            // Find first element >= nums[i]
+            int replaceIndex =
+                lowerBound(tails, 0, lisEndIndex, nums[i]);
+
+            // Replace with smaller tail
+            tails[replaceIndex] = nums[i];
         }
 
-        return ind + 1;
+        return lisEndIndex + 1;
     }
 
-    public int firstNoGreater(int[] nums, int i, int j, int target)
-    {
-        int low = i;
-        int high = j;
-        int resultIndex = Integer.MAX_VALUE;
-        while(low <= high)
-        {
-            int mid = (low + high) / 2;
-            if(nums[mid] >= target)
-            {    
-                resultIndex = Math.min(resultIndex, mid);
-                high = mid - 1;
+    public int lowerBound(
+        int[] nums,
+        int left,
+        int right,
+        int target
+    ) {
+
+        while(left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] >= target) {
+                right = mid - 1;
             }
-            else
-            {
-                low = mid + 1;
+            else {
+                left = mid + 1;
             }
         }
 
-        return resultIndex;
+        return left;
     }
 }
