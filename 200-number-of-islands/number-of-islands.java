c1890e8f7f1bf7a9d7c11d41f3fa0eb6347e412f
prithvikota31 <prithvikota@microsoft.com>
2026-05-16 16:27:24 -0700

    Time: 4 ms (43.00%) | Memory: 52.3 MB (59.84%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -4,38 +4,36 @@ class Solution {
         int n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
         int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0, 1, 0, -1};
+        int[] delCol = {0, -1, 0, 1};
         int count = 0;
-
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
             {
                 if(!visited[i][j] && grid[i][j] == '1')
                 {
-                    dfs(grid, visited, i, j, delRow, delCol);
+                    dfs(visited, grid, delRow, delCol, i, j);
                     count++;
                 }
             }
         }
-
         return count;
+        
     }
 
-
-    public void dfs(char[][] grid, boolean[][] visited , int r, int c, int[] delRow, int[] delCol)
+    public void dfs(boolean[][] visited, char[][] grid, int[] delRow, int[] delCol, int row, int col)
     {
-        visited[r][c] = true;
+        visited[row][col] = true;
 
-        for(int i = 0; i < 4; i++)
+        for(int i = 0; i < delRow.length; i++)
         {
-            int nRow = delRow[i] + r;
-            int nCol = delCol[i] + c;
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
 
             if(nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length &&
                 !visited[nRow][nCol] && grid[nRow][nCol] == '1')
             {
-                dfs(grid, visited, nRow, nCol, delRow, delCol);
+                dfs(visited, grid, delRow, delCol, nRow, nCol);
             }
         }
     }
