11648cac74a00fe99315680cf2c58b0564c3f7af
prithvikota31 <prithvikota@microsoft.com>
2026-05-16 16:34:15 -0700

    Time: 2 ms (63.35%) | Memory: 46.1 MB (97.41%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.java
@@ -0,0 +1,42 @@
+class Solution {
+    private int maxArea = 0;
+    public int maxAreaOfIsland(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, -1, 0, 1};
+        int[] count = new int[1];
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(!visited[i][j] && grid[i][j] == 1)
+                {
+                    count[0] = 0;
+                    dfs(visited, grid, delRow, delCol, i, j, count);
+                    maxArea = Math.max(maxArea, count[0]);
+                }
+            }
+        }
+        return maxArea;
+        
+    }
+
+    public void dfs(boolean[][] visited, int[][] grid, int[] delRow, int[] delCol, int row, int col, int[] count)
+    {
+        visited[row][col] = true;
+        count[0]++;
+        for(int i = 0; i < delRow.length; i++)
+        {
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
+
+            if(nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length &&
+                !visited[nRow][nCol] && grid[nRow][nCol] == 1)
+            {
+                dfs(visited, grid, delRow, delCol, nRow, nCol, count);
+            }
+        }
+    }
+}
