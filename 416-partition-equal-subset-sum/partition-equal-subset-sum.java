7287a072aa1115fd3af781ca149d7c19b659dc6c
prithvikota31 <prithvikota@microsoft.com>
2026-05-15 17:12:05 -0700

    Time: 51 ms (44.19%) | Memory: 47.7 MB (45.69%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        
+        int sum = 0;
+        for(int num: nums)
+        {
+            sum += num;
+        }
+
+        if(sum % 2 != 0)    return false;
+
+        int target = sum / 2;
+
+        return targetSum(nums, target);
+
+
+    }
+
+    public boolean targetSum(int[] nums, int target)
+    {
+        boolean[][] dp = new boolean[nums.length][target + 1];
+        int m = dp.length;
+        int n = dp[0].length; //target
+
+        //for target 0, its possible
+        for(int i = 0; i < m; i++)
+        {
+            dp[i][0] = true;
+        }
+
+        if(target >= nums[0])
+        {
+            dp[0][nums[0]] = true;
+        }
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                //include and not include, j is target
+                boolean notInclude = dp[i - 1][j];
+                boolean include = false;
+                if(j >= nums[i])
+                    include = dp[i - 1][j - nums[i]];
+                dp[i][j] = include || notInclude;
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
