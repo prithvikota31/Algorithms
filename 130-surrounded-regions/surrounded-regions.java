5b768ed0bda795e7d388b9711f920e9647aab16b
prithvikota31 <prithvikota@microsoft.com>
2026-05-16 19:59:28 -0700

    Time: 2 ms (75.85%) | Memory: 47.7 MB (72.85%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,49 +1,54 @@
 class Solution {
-    int m, n;
-    int[] dr = {-1, 0, 1, 0};
-    int[] dc = {0, -1, 0, 1};
-
     public void solve(char[][] board) {
-        if (board.length == 0) return;
-        m = board.length;
-        n = board[0].length;
-
+        int m = board.length;
+        int n = board[0].length;
         boolean[][] visited = new boolean[m][n];
 
-        for (int i = 1; i < m - 1; i++) {
-            for (int j = 1; j < n - 1; j++) {
-                if (board[i][j] == 'O' && !visited[i][j]) {
-                    List<int[]> comp = new ArrayList<>();
-                    boolean touchesBorder = dfs(board, visited, i, j, comp);
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, -1, 0, 1};
 
-                    if (!touchesBorder) {
-                        for (int[] p : comp) {
-                            board[p[0]][p[1]] = 'X';
-                        }
-                    }
+        //mark all cells reachable from edge 'O's
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if((i == 0 || i == m - 1 || j == 0 || j == n - 1) && board[i][j] == 'O')
+                {
+                    //dfs
+                    dfs(visited, board, delRow, delCol, i, j);
+
                 }
             }
         }
-    }
 
-    private boolean dfs(char[][] board, boolean[][] visited,
-                        int r, int c, List<int[]> comp) {
-
-        visited[r][c] = true;
-        comp.add(new int[]{r, c});
-
-        boolean touchesBorder =
-                (r == 0 || r == m - 1 || c == 0 || c == n - 1);
-
-        for (int k = 0; k < 4; k++) {
-            int nr = r + dr[k];
-            int nc = c + dc[k];
-
-            if (nr >= 0 && nr < m && nc >= 0 && nc < n
-                    && !visited[nr][nc] && board[nr][nc] == 'O') {
-                touchesBorder |= dfs(board, visited, nr, nc, comp);
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(!visited[i][j] && board[i][j] == 'O')
+                {
+                    board[i][j] = 'X';
+                }
             }
         }
-        return touchesBorder;
+
     }
+
+    public void dfs(boolean[][] visited, char[][] grid, int[] delRow, int[] delCol, int row, int col)
+    {
+        visited[row][col] = true;
+        for(int i = 0; i < delRow.length; i++)
+        {
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
+
+            if(nRow >= 0 && nRow < visited.length && nCol >= 0 && nCol < visited[0].length && !visited[nRow][nCol]
+                && grid[nRow][nCol] == 'O')
+            {
+                dfs(visited, grid, delRow, delCol, nRow, nCol);
+            }
+        }
+    }
+
+
 }
