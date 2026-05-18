54bbef4d963ae3c25f688e44199c76b28b350d20
prithvikota31 <prithvikota@microsoft.com>
2026-05-18 16:45:09 -0700

    Time: 130 ms (88.44%) | Memory: 108.3 MB (64.77%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,56 +1,53 @@
 class TimeMap {
+
     Map<String, List<Pair>> map;
-
-
     public TimeMap() {
         map = new HashMap<>();
-        
     }
     
     public void set(String key, String value, int timestamp) {
         map.computeIfAbsent(key, k -> new ArrayList<>()).add(new Pair(value, timestamp));
-        
     }
     
     public String get(String key, int timestamp) {
-        if(!map.containsKey(key))
-        {
-            return "";
-        }
+        if(!map.containsKey(key))   return "";
+
         List<Pair> list = map.get(key);
-
-        int l = 0;
-        int h = list.size() - 1;
-
-        String ans = "";
-
-        while(l <= h)
+        int low = 0;
+        int high = list.size() - 1;
+        int ans = Integer.MIN_VALUE;
+        while(low <= high)
         {
-            int mid = l + (h-l)/2;
-
-            if(list.get(mid).time <= timestamp)
+            int mid = low + (high - low) / 2;
+            if(list.get(mid).timestamp <= timestamp)
             {
-                ans = list.get(mid).val;
-                l = mid + 1;
+                ans = Math.max(ans, mid);
+                low = mid + 1;
             }
             else
             {
-                h = mid - 1;
+                high = mid - 1;
             }
         }
 
-        return ans;
-        
-    }
-
-    class Pair{
-        String val;
-        int time;
-        public Pair(String val, int time)
+        if(ans == Integer.MIN_VALUE)
         {
-            this.val = val;
-            this.time = time;
+           return ""; 
         }
+        else
+        {
+            return list.get(ans).value;
+        }
+    }
+}
+
+class Pair{
+    String value;
+    int timestamp;
+    public Pair(String value, int timestamp)
+    {
+        this.timestamp = timestamp;
+        this.value = value;
     }
 }
 
