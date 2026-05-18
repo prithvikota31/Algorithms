8ed7ed43fc136ffdd9f6995ec5a9e48b4c16e0a5
prithvikota31 <prithvikota@microsoft.com>
2026-05-17 23:25:39 -0700

    Time: 3 ms (61.83%) | Memory: 46.8 MB (32.87%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,43 +1,64 @@
 class Solution {
+    private List<Integer> size, parent;
     public int countComponents(int n, int[][] edges) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i < n; i++)
+        size = new ArrayList<>();
+        parent = new ArrayList<>();
+        for(int i = 0; i <= n; i++)
         {
-            graph.add(new ArrayList<>());
+            size.add(1);
+            parent.add(i);
         }
+
+        int numConnected = n; //start with n
+
 
         for(int i = 0; i < edges.length; i++)
         {
-            graph.get(edges[i][0]).add(edges[i][1]);
-            graph.get(edges[i][1]).add(edges[i][0]);
-        }
-
-        int count = 0;
-        boolean[] visited = new boolean[n];
-        for(int i = 0; i < visited.length; i++)
-        {
-            if(!visited[i])
+            if(unionBySize(edges[i][0], edges[i][1]))
             {
-                //dfs
-                dfs(i, visited, graph);
-                count++;
+                numConnected--;
             }
         }
 
-        return count;
+        return numConnected;
     }
 
 
-    public void dfs(int node, boolean[] visited, List<List<Integer>> graph)
+    private int findUParent(int node)
     {
-        visited[node] = true;
-
-        for(int v : graph.get(node))
+        if(parent.get(node) == node)
         {
-            if(!visited[v])
-            {
-                dfs(v, visited, graph);
-            }
+            return node;
         }
+
+        int ulp = findUParent(parent.get(node));
+        parent.set(node, ulp);
+        return parent.get(node);
     }
+
+    private boolean unionBySize(int u, int v)
+    {
+        int ulPu = findUParent(u);
+        int ulPv = findUParent(v);
+        if(ulPu == ulPv)
+        {
+            return false;
+        }
+
+        if(size.get(ulPu) <= size.get(ulPv))
+        {
+            parent.set(ulPu, ulPv);
+            size.set(ulPv, size.get(ulPu) + size.get(ulPv));
+        }
+        else
+        {
+            parent.set(ulPv, ulPu);
+            size.set(ulPu, size.get(ulPu) + size.get(ulPv));
+        }
+
+        return true;
+    }
+
+
+
 }
