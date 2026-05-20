61af5f43829b0cd6d2eb7c9af1f9bfdc05be1f35
prithvikota31 <prithvikota@microsoft.com>
2026-05-19 18:31:30 -0700

    Time: 1 ms (72.02%) | Memory: 43.7 MB (48.86%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,32 +14,33 @@
  * }
  */
 class Solution {
-    
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        if(root == null)
-        {
-            return ans;
-        }
-
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
+        
+        List<Integer> result = new ArrayList<>();
+        if(root == null)    return result;
+        Deque<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
 
         while(!q.isEmpty())
         {
-            int l = q.size();
-
-            for(int i = 0; i < l; i++)
+            int size = q.size();
+            for(int i = 0; i < size; i++)
             {
-                TreeNode node = q.poll();
-                if(i == l-1)
+                TreeNode cur = q.poll();
+                if(i == size - 1)   result.add(cur.val); 
+                if(cur.left != null)
                 {
-                    ans.add(node.val);
+                    q.offer(cur.left);
                 }
-                if(node.left != null) q.add(node.left);
-                if(node.right != null) q.add(node.right);
+                if(cur.right != null)
+                {
+                    q.offer(cur.right);
+                }
             }
+
+
         }
-        return ans;
+
+        return result;
     }
 }
