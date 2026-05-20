dfe86e6b802c704bcf78f07647a1ad0953b30597
prithvikota31 <prithvikota@microsoft.com>
2026-05-19 18:06:53 -0700

    Time: 3 ms (67.46%) | Memory: 46.5 MB (32.70%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,21 +15,36 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null && subRoot == null) return true;
         if(root == null)    return false;
-        if(isSameTree(root, subRoot))   return true;
 
-        return (isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot));
+        if(root.val == subRoot.val)
+        {
+            if(isSametree(root, subRoot))
+            {
+                return true;
+            }
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+        
     }
 
-
-    private boolean isSameTree(TreeNode root1, TreeNode root2)
+    public boolean isSametree(TreeNode root1, TreeNode root2)
     {
-        if(root1 == null && root2 == null)  return true;
-        else if(root1 == null || root2 == null) return false;
+        if(root1 == null && root2 == null)
+        {
+            return true;
+        }
+        if(root1 == null || root2 == null)
+        {
+            return false;
+        }
+        if(root1.val != root2.val)
+        {
+            return false;
+        }
 
-        if(root1.val != root2.val)  return false;
-
-        return isSameTree(root1.left, root2.left) &&  isSameTree(root1.right, root2.right);
-
+        return isSametree(root1.left, root2.left) && isSametree(root1.right, root2.right);
     }
 }
