18c45d91d8aaf5c20d625cb3d28883d115dee758
prithvikota31 <prithvikota@microsoft.com>
2026-06-01 21:53:32 -0700

    Time: 29 ms (69.97%) | Memory: 53.7 MB (78.70%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,26 +1,23 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int[][] result = new int[k][2];
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) ->
+                                     ((b[1] * b[1] + b[0] * b[0]) - (a[1] * a[1] + a[0] * a[0])));
 
-        // use minheap to keep k size and k closest points
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) 
-                                    -> ((b[1] * b[1] + b[0] * b[0]) - (a[1] * a[1] + a[0] * a[0])));
-        
         for(int[] point: points)
         {
-            pq.offer(point);
-            if(pq.size() > k)
+            maxHeap.offer(point);
+
+            if(maxHeap.size() > k)
             {
-                pq.poll();
+                maxHeap.poll();
             }
         }
-
+        int[][] ans = new int[k][2];
         for(int i = 0; i < k; i++)
         {
-            result[i] = pq.poll();
+            ans[i] = maxHeap.poll();
         }
 
-        return result;
-
+        return ans;
     }
 }
