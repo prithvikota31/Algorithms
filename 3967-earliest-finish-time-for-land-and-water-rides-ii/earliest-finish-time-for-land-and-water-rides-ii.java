0ab9284d9a3f9454508381f7b6f38b40bc653ec8
prithvikota31 <prithvikota@microsoft.com>
2026-06-02 19:15:45 -0700

    Time: 2 ms (100.00%) | Memory: 92.2 MB (39.66%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.java
@@ -1,43 +1,27 @@
 class Solution {
+    public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int landFirst = solve(landStartTime, landDuration, waterStartTime, waterDuration);
+        int waterFirst = solve(waterStartTime, waterDuration, landStartTime, landDuration);
 
-    private int solve(
-        int[] start1,
-        int[] duration1,
-        int[] start2,
-        int[] duration2
-    ) {
-        int finish1 = Integer.MAX_VALUE;
-        for (int i = 0; i < start1.length; i++) {
-            finish1 = Math.min(finish1, start1[i] + duration1[i]);
-        }
-        int finish2 = Integer.MAX_VALUE;
-        for (int i = 0; i < start2.length; i++) {
-            finish2 = Math.min(
-                finish2,
-                Math.max(start2[i], finish1) + duration2[i]
-            );
-        }
-        return finish2;
+        return Math.min(landFirst, waterFirst);
     }
 
-    public int earliestFinishTime(
-        int[] landStartTime,
-        int[] landDuration,
-        int[] waterStartTime,
-        int[] waterDuration
-    ) {
-        int land_water = solve(
-            landStartTime,
-            landDuration,
-            waterStartTime,
-            waterDuration
-        );
-        int water_land = solve(
-            waterStartTime,
-            waterDuration,
-            landStartTime,
-            landDuration
-        );
-        return Math.min(land_water, water_land);
+    private int solve(int[] start1, int[] duration1, int[] start2, int[] duration2)
+    {
+        //earliest finish time of set 1
+        int finished1 = Integer.MAX_VALUE;
+        for(int i = 0; i < start1.length; i++)
+        {
+            finished1 = Math.min(finished1, start1[i] + duration1[i]);
+        }
+
+        //now get second ride best possibility
+        int finished2 = Integer.MAX_VALUE;
+        for(int i = 0; i < start2.length; i++)
+        {
+            finished2 = Math.min(finished2, (Math.max(finished1, start2[i]) +  duration2[i]));
+        }
+
+        return finished2;
     }
 }
