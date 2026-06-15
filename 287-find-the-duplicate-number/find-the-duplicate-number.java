1a9cf63c83fa3ce56182e21b108eeaa07b181c06
prithvikota31 <prithvikota@microsoft.com>
2026-06-15 00:41:56 -0700

    Time: 4 ms (90.90%) | Memory: 82.9 MB (57.15%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -2,14 +2,14 @@ class Solution {
 
     public int findDuplicate(int[] nums) {
 
-        int slow = 0;
-        int fast = 0;
+        int slow = nums[0];
+        int fast = nums[nums[0]];
 
         // Phase 1: detect cycle
-        do {
+        while (slow != fast) {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        } while (slow != fast);
+        } 
 
         // Phase 2: find cycle entry
         slow = 0;
