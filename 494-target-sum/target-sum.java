86039d9958070919b6cccc7d7d6760879be854fa
prithvikota31 <prithvikota@microsoft.com>
2026-06-19 05:37:41 -0700

    Time: 6 ms (71.78%) | Memory: 44.8 MB (49.30%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,45 +1,41 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        //sum
-        //s1 - s2 = target
-        //s1 + s2 = sum
-
-        //s1 = target + sum / 2
         int sum = 0;
-        for(int num: nums)
-        {
+        for (int num : nums) {
             sum += num;
         }
 
-        if((target + sum) % 2 !=  0 || sum < Math.abs(target))
-        {
+        // We need s1 - s2 = target and s1 + s2 = sum.
+        // So s1 = (target + sum) / 2.
+        if ((target + sum) % 2 != 0 || sum < Math.abs(target)) {
             return 0;
         }
-        int newTarget = (target + sum) / 2;
+
+        int subsetTarget = (target + sum) / 2;
+        return countSubsets(nums, subsetTarget);
+    }
+
+    private int countSubsets(int[] nums, int target) {
         int m = nums.length;
-        int n = newTarget;
+        int[][] dp = new int[m + 1][target + 1];
 
-        int[][] dp = new int[m + 1][n + 1];
+        // With 0 elements, there is exactly 1 way to make sum 0: choose nothing.
+        dp[0][0] = 1;
 
-        //0th row implies no elements;
-        dp[0][0] = 1; //no elemeents target 0
+        // dp[i][j] = number of ways to make sum j using first i elements.
+        for (int i = 1; i <= m; i++) {
+            for (int j = 0; j <= target; j++) {
 
-        for(int i = 1; i < dp.length; i++)
-        {
-            for(int j = 0; j < dp[0].length; j++)
-            {
-                //dont use current element
-                //notInclude
-                dp[i][j]  += dp[i - 1][j];
-                //include
-                if(j >= nums[i - 1])
-                    dp[i][j] += dp[i - 1][j - nums[i - 1]]; 
+                // Do not use current element nums[i - 1].
+                dp[i][j] += dp[i - 1][j];
+
+                // Use current element nums[i - 1], if it fits.
+                if (j >= nums[i - 1]) {
+                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
+                }
             }
         }
 
-        return dp[m][n];
-
-
-
+        return dp[m][target];
     }
 }
