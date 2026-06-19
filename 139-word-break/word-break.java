1a1aadf3c525c246d7e32ebf169488ce4025e30c
prithvikota31 <prithvikota@microsoft.com>
2026-06-19 04:41:58 -0700

    Time: 8 ms (51.83%) | Memory: 46.1 MB (47.95%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         int l = s.length();
+
+        HashSet<String> set = new HashSet<>(wordDict);
+
         boolean[] dp = new boolean[l+1];
-
-        Set<String> set = new HashSet<>(wordDict);
-
         dp[0] = true;
 
         for(int i = 1; i <= l; i++)
         {
-            for(int j = 0; j <= i; j++)
+            for(int j = 0; j < i; j++)
             {
                 if(dp[j] && set.contains(s.substring(j, i)))
                 {
@@ -19,9 +19,6 @@ class Solution {
             }
         }
 
-        return dp[l];
-
-
-        
+        return dp[l]; 
     }
 }
