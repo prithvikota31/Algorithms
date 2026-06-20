8c004fe3eeaa882b6f50a704c9c8fe61c09d78af
prithvikota31 <prithvikota@microsoft.com>
2026-06-20 13:37:29 -0700

    Time: 0 ms (100.00%) | Memory: 48 MB (56.67%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,34 +1,33 @@
 class Solution {
     public double myPow(double x, int n) {
         long N = n;
-
         if(N < 0)
         {
             N = -N;
-            x = 1.0/x;
+            x = (1.0 / x);
         }
 
-        return myPowPositive(x, N);  
+        return powCal(x, N);
     }
 
-    public double myPowPositive(double x, long N)
-    {
-        if(N == 1)
+    private double powCal(double x, long n) // x > 0 and n +ve
+    {   
+        if(n == 0){
+            return 1;
+        }
+        if(n == 1)
         {
             return x;
         }
-        if(N == 0)
-        {
-            return 1;
-        }
 
-        if(N%2 == 1)
+        if(n % 2 == 0)
         {
-            return x * myPowPositive(x*x, (N-1)/2);
+            return powCal(x * x, n / 2);
         }
         else
         {
-            return myPowPositive(x*x, N/2);
+            return x * powCal(x * x, (n - 1) / 2);
         }
+
     }
 }
