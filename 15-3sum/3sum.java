a0de353ee43f0e8adbbd2915f07334b7e255acb8
prithvikota31 <prithvikota@microsoft.com>
2026-06-20 01:38:05 -0700

    Time: 43 ms (19.11%) | Memory: 53.5 MB (99.74%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,52 +1,48 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        Arrays.sort(nums);
-
         List<List<Integer>> result = new ArrayList<>();
-
         int n = nums.length;
-
-        for(int i = 0; i < n - 2; i++) {
-
-            // skip duplicate first elements
-            if(i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
+        Arrays.sort(nums);
+        // 1, 1, 2, 2, 3, 4, 5, 6
+        for(int i = 0; i <= n - 3; i++)
+        {
+            if(i > 0 && nums[i] == nums[i - 1])  continue;
             int j = i + 1;
             int k = n - 1;
 
-            while(j < k) {
+            while(j < k)
+            {
+                if(j > i + 1 && nums[j] == nums[j - 1])  
+                {
+                    j++;
+                    continue;
+                }
+               
+                if(k < n - 1 && nums[k] == nums[k + 1])  
+                {
+                    k--;
+                    continue;
+                }
+                
 
                 int sum = nums[i] + nums[j] + nums[k];
-
-                if(sum == 0) {
-
-                    result.add(
-                        Arrays.asList(nums[i], nums[j], nums[k])
-                    );
-
-                    j++;
+                if(sum > 0 )
+                {
                     k--;
-
-                    // skip duplicate j
-                    while(j < k && nums[j] == nums[j - 1]) {
-                        j++;
-                    }
-
                 }
-
-                else if(sum < 0) {
+                else if(sum < 0)
+                {
                     j++;
                 }
-
-                else {
+                else
+                {
+                    result.add(new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[k])));
+                    j++;
                     k--;
                 }
             }
         }
-
         return result;
+
     }
 }
