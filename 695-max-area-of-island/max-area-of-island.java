7e11203f90f08b1752702c2f862738783d25cb7e
prithvikota31 <prithvikota@microsoft.com>
2026-06-21 01:34:17 -0700

    Time: 2 ms (63.99%) | Memory: 46.4 MB (83.37%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,6 +1,6 @@
 class Solution {
-    private int maxArea = 0;
-    private int area = 0;
+
+
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -10,7 +10,8 @@ class Solution {
 
         int[][] visited = new int[m][n];
 
-        int count = 0;
+        int area = 0;
+        int maxArea = 0;
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
@@ -18,7 +19,7 @@ class Solution {
                 if(visited[i][j] == 0 && grid[i][j] == 1)
                 {
                     area = 0;
-                    dfs(grid, visited, i, j, delRow, delCol);
+                    area = dfs(grid, visited, i, j, delRow, delCol);
                     maxArea = Math.max(maxArea, area);
                 }
             }
@@ -26,10 +27,10 @@ class Solution {
         return maxArea;
     }
 
-    private void dfs(int[][] grid, int[][] visited, int row, int col, int[] delRow, int[] delCol)
+    private int dfs(int[][] grid, int[][] visited, int row, int col, int[] delRow, int[] delCol)
     {
         visited[row][col] = 1;
-        area++;
+        int area = 1;
         for(int i = 0; i < 4; i++)
         {
             int nRow = row + delRow[i];
@@ -38,8 +39,10 @@ class Solution {
             if(nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length 
                 && grid[nRow][nCol] == 1 && visited[nRow][nCol] == 0)
             {
-                dfs(grid, visited, nRow, nCol, delRow, delCol);
+                area += dfs(grid, visited, nRow, nCol, delRow, delCol);
             }
         }
+
+        return area;
     }
 }
