482bf1cb0d6c2d58d869d85ec6810e7a35645919
prithvikota31 <prithvikota@microsoft.com>
2026-06-21 02:07:08 -0700

    Time: 6 ms (98.05%) | Memory: 50.7 MB (17.12%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.java
+++ 286-walls-and-gates/walls-and-gates.java
@@ -3,55 +3,38 @@ class Solution {
         int m = rooms.length;
         int n = rooms[0].length;
 
-        Deque<Pair> q = new ArrayDeque<>();
-
+        Deque<int[]> q = new ArrayDeque<>();
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
             {
                 if(rooms[i][j] == 0)
                 {
-                    q.offer(new Pair(i, j));
+                    q.offer(new int[]{i, j});
                 }
             }
         }
-        int[] delRow = {0, 1, 0, -1};
-        int[] delCol = {1, 0, -1, 0};
+
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, -1, 0, 1};
 
         while(!q.isEmpty())
         {
-            Pair cur = q.poll();
-            int curRow = cur.row;
-            int curCol = cur.col;
+            int[] cur = q.poll();
+            int curRow = cur[0];
+            int curCol = cur[1];
 
             for(int i = 0; i < 4; i++)
             {
                 int nRow = curRow + delRow[i];
                 int nCol = curCol + delCol[i];
 
-                if(nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && 
-                    rooms[nRow][nCol] == Integer.MAX_VALUE)
+                if(nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && rooms[nRow][nCol] == Integer.MAX_VALUE)
                 {
-                    q.offer(new Pair(nRow, nCol));
-                    rooms[nRow][nCol] = 1 + rooms[curRow][curCol];
+                    rooms[nRow][nCol] = rooms[curRow][curCol] + 1;
+                    q.offer(new int[]{nRow, nCol});
                 }
             }
         }
-
-        return;
-    }
-
-
-
-}
-
-class Pair{
-    int row;
-    int col;
-    int distance;
-    public Pair(int r, int c)
-    {
-        this.row = r;
-        this.col = c;
     }
 }
