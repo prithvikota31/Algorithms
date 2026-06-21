f2a4e73f23307741efb19d644a1ef822cb4f3871
prithvikota31 <prithvikota@microsoft.com>
2026-06-21 01:27:49 -0700

    Time: 2 ms (63.99%) | Memory: 46.6 MB (35.20%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,42 +1,44 @@
 class Solution {
-    
+    private int maxArea = 0;
+    private int area = 0;
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int[] delRow = {-1, 0, 1, 0};
-        int[] delCol = {0, -1, 0, 1};
-        int[] count = new int[1];
-        int maxArea = 0;
+
+        int[] delRow = {0, 1, 0, -1};
+        int[] delCol = {1, 0, -1, 0};
+
+        int[][] visited = new int[m][n];
+
+        int count = 0;
         for(int i = 0; i < m; i++)
         {
             for(int j = 0; j < n; j++)
             {
-                if(!visited[i][j] && grid[i][j] == 1)
+                if(visited[i][j] == 0 && grid[i][j] == 1)
                 {
-                    count[0] = 0;
-                    dfs(visited, grid, delRow, delCol, i, j, count);
-                    maxArea = Math.max(maxArea, count[0]);
+                    area = 0;
+                    dfs(grid, visited, i, j, delRow, delCol);
+                    maxArea = Math.max(maxArea, area);
                 }
             }
         }
         return maxArea;
-        
     }
 
-    public void dfs(boolean[][] visited, int[][] grid, int[] delRow, int[] delCol, int row, int col, int[] count)
+    private void dfs(int[][] grid, int[][] visited, int row, int col, int[] delRow, int[] delCol)
     {
-        visited[row][col] = true;
-        count[0]++;
-        for(int i = 0; i < delRow.length; i++)
+        visited[row][col] = 1;
+        area++;
+        for(int i = 0; i < 4; i++)
         {
             int nRow = row + delRow[i];
             int nCol = col + delCol[i];
 
-            if(nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length &&
-                !visited[nRow][nCol] && grid[nRow][nCol] == 1)
+            if(nRow >= 0 && nRow < grid.length && nCol >= 0 && nCol < grid[0].length 
+                && grid[nRow][nCol] == 1 && visited[nRow][nCol] == 0)
             {
-                dfs(visited, grid, delRow, delCol, nRow, nCol, count);
+                dfs(grid, visited, nRow, nCol, delRow, delCol);
             }
         }
     }
