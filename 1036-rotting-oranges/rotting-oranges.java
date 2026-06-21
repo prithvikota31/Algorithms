2105ff12346304c363987f375093ed9e659a585b
prithvikota31 <prithvikota@microsoft.com>
2026-06-21 02:46:43 -0700

    Time: 2 ms (90.90%) | Memory: 44.1 MB (56.01%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -3,17 +3,19 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        Deque<int[]> q = new ArrayDeque<>();
         int fresh = 0;
-
-        // Put all initially rotten oranges into queue.
-        // This is multi-source BFS.
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    q.offer(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
+        Deque<int[]> q = new ArrayDeque<>(); //contains rotten
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j] == 1)
+                {
                     fresh++;
+                }
+                else if(grid[i][j] == 2)
+                {
+                    q.offer(new int[]{i, j});
                 }
             }
         }
@@ -22,32 +24,41 @@ class Solution {
         int[] delCol = {-1, 0, 1, 0};
 
         int time = 0;
-
-        // Each BFS level = 1 minute.
-        while (!q.isEmpty() && fresh > 0) {
+        while(!q.isEmpty())
+        {
             int size = q.size();
 
-            for (int i = 0; i < size; i++) {
+            for(int i = 0; i < size; i++)
+            {
                 int[] cur = q.poll();
 
-                for (int d = 0; d < 4; d++) {
-                    int nRow = cur[0] + delRow[d];
-                    int nCol = cur[1] + delCol[d];
-
-                    if (nRow >= 0 && nRow < m &&
-                        nCol >= 0 && nCol < n &&
-                        grid[nRow][nCol] == 1) {
-
+                for(int j = 0; j < 4; j++)
+                {
+                    int nRow = cur[0] + delRow[j];
+                    int nCol = cur[1] + delCol[j];
+                    if(nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && grid[nRow][nCol] == 1)
+                    {
                         grid[nRow][nCol] = 2;
-                        fresh--;
                         q.offer(new int[]{nRow, nCol});
+                        fresh--;
                     }
                 }
             }
 
-            time++;
+            if(!q.isEmpty())
+            {
+                time++;
+            }
         }
 
-        return fresh == 0 ? time : -1;
+        if(fresh == 0)
+        {
+            return time;
+        }
+        else
+        {
+            return -1;
+        }
+
     }
 }
