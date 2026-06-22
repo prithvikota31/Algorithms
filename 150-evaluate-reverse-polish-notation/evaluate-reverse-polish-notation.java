7de4f36b8a82ede1999af4858fb7e8344fabc375
prithvikota31 <prithvikota@microsoft.com>
2026-06-22 00:24:55 -0700

    Time: 7 ms (36.15%) | Memory: 45.5 MB (16.53%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,62 +1,26 @@
-// class Solution {
-//     public int evalRPN(String[] tokens) {
-//         Deque<Integer> stack = new ArrayDeque<>();
-//         for(String s: tokens)
-//         {
-
-//             if(s.equals("*") || s.equals("/") || s.equals("-") || s.equals("+"))
-//             {
-//                 int op1 = stack.pop();
-//                 int op2 = stack.pop();
-//                 int result;
-//                 switch(s){
-//                     case "*":
-//                         result = op2 * op1;
-//                         stack.push(result);
-//                         break;
-//                     case "+":
-//                         result = op2 + op1;
-//                         stack.push(result);
-//                         break;
-//                     case "-":
-//                         result = op2 - op1;
-//                         stack.push(result);
-//                     break;
-//                     default:
-//                         result = op2 / op1;
-//                         stack.push(result);
-//                 }
-//             }
-//             else
-//             {
-//                 stack.push(Integer.valueOf(s));
-//             }
-//         }
-
-//         return stack.peek();
-//     }
-// }
-
 class Solution {
     public int evalRPN(String[] tokens) {
-        Deque<Integer> st = new ArrayDeque<>();
 
-        for (String t : tokens) {
-            switch (t) {
-                case "+" -> st.push(st.pop() + st.pop());
-                case "-" -> {
-                    int b = st.pop(), a = st.pop();
-                    st.push(a - b);
-                }
-                case "*" -> st.push(st.pop() * st.pop());
-                case "/" -> {
-                    int b = st.pop(), a = st.pop();
-                    st.push(a / b);
-                }
-                default -> st.push(Integer.parseInt(t));
+        Stack<Integer> stack = new Stack<>();
+
+       for(String t : tokens)
+       {
+            if(t.equals("+") || t.equals("-") || t.equals("*") || t.equals("/"))
+            {
+                int b = stack.pop();
+                int a = stack.pop();
+
+                if(t.equals("+")) stack.push(a + b);
+                else if(t.equals("-")) stack.push(a - b);
+                else if(t.equals("*")) stack.push(a * b);
+                else stack.push(a / b); // truncates toward zero
             }
-        }
-        return st.peek();
-    }
+            else
+            {
+                stack.push(Integer.parseInt(t));
+            }
+       }
 
+       return stack.pop();
+    }
 }
