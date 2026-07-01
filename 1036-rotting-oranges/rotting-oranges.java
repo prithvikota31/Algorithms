894f2d29fb569c6f3a81d2d263bc57fdb0b4a43a
prithvikota31 <prithvikota@microsoft.com>
2026-07-01 00:11:00 -0700

    Time: 2 ms (90.81%) | Memory: 44.1 MB (56.34%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -4,18 +4,16 @@ class Solution {
         int n = grid[0].length;
 
         int fresh = 0;
-        Deque<int[]> q = new ArrayDeque<>(); //contains rotten
-        for(int i = 0; i < m; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                if(grid[i][j] == 1)
-                {
+        Deque<int[]> q = new ArrayDeque<>(); // {row, col, time}
+
+        // Add all rotten oranges as BFS sources at time = 0.
+        // Count fresh oranges so we know whether all can be rotted.
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
                     fresh++;
-                }
-                else if(grid[i][j] == 2)
-                {
-                    q.offer(new int[]{i, j});
+                } else if (grid[i][j] == 2) {
+                    q.offer(new int[]{i, j, 0});
                 }
             }
         }
@@ -24,41 +22,33 @@ class Solution {
         int[] delCol = {-1, 0, 1, 0};
 
         int time = 0;
-        while(!q.isEmpty())
-        {
-            int size = q.size();
 
-            for(int i = 0; i < size; i++)
-            {
-                int[] cur = q.poll();
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
 
-                for(int j = 0; j < 4; j++)
-                {
-                    int nRow = cur[0] + delRow[j];
-                    int nCol = cur[1] + delCol[j];
-                    if(nRow >= 0 && nRow < m && nCol >= 0 && nCol < n && grid[nRow][nCol] == 1)
-                    {
-                        grid[nRow][nCol] = 2;
-                        q.offer(new int[]{nRow, nCol});
-                        fresh--;
-                    }
+            int curRow = cur[0];
+            int curCol = cur[1];
+            int curTime = cur[2];
+
+            // Track the farthest minute reached by BFS.
+            time = curTime;
+
+            for (int i = 0; i < 4; i++) {
+                int nRow = curRow + delRow[i];
+                int nCol = curCol + delCol[i];
+
+                // Fresh orange becomes rotten at curTime + 1.
+                if (nRow >= 0 && nRow < m &&
+                    nCol >= 0 && nCol < n &&
+                    grid[nRow][nCol] == 1) {
+
+                    grid[nRow][nCol] = 2;
+                    fresh--;
+                    q.offer(new int[]{nRow, nCol, curTime + 1});
                 }
             }
-
-            if(!q.isEmpty())
-            {
-                time++;
-            }
         }
 
-        if(fresh == 0)
-        {
-            return time;
-        }
-        else
-        {
-            return -1;
-        }
-
+        return fresh == 0 ? time : -1;
     }
 }
