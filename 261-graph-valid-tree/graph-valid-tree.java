bc6611db825b7bf4523f4bedf23c15b35e616d95
prithvikota31 <prithvikota@microsoft.com>
2026-07-01 00:59:13 -0700

    Time: 2 ms (59.78%) | Memory: 46.2 MB (92.42%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -1,54 +1,41 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
-        List<List<Integer>> graph = new ArrayList<>();
-            if (edges.length != n - 1) return false;
-            for(int i = 0; i < n; i++)
-            {
-                graph.add(new ArrayList<>());
-            }
-
-            for(int i = 0; i < edges.length; i++)
-            {
-                graph.get(edges[i][0]).add(edges[i][1]);
-                graph.get(edges[i][1]).add(edges[i][0]);
-            }
-
-            int[] visited = new int[n];
-            Deque<int[]> q = new ArrayDeque<>();
-            q.offer(new int[]{0, -1});
-            visited[0] = 1;
-
-
-            while(!q.isEmpty())
-            {
-                int[] curNode = q.poll();
-                int curValue = curNode[0];
-                int curParent = curNode[1];
-                for(int nei: graph.get(curValue))
-                {
-                    if(visited[nei] == 0)
-                    {
-                        q.offer(new int[]{nei, curValue});
-                        visited[nei] = 1;
-                    }
-                    else if(nei != curParent)
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            for(int i = 0; i < n; i++)
-            {
-                if(visited[i] == 0)
-                {
-                    return false;
-                }
-            }
-
-            return true;
-
-
+        if (edges.length != n - 1) {
+            return false;
         }
-    }
 
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+        Deque<Integer> q = new ArrayDeque<>();
+
+        q.offer(0);
+        visited[0] = true;
+
+        int count = 0;
+
+        // BFS checks whether all nodes are connected.
+        while (!q.isEmpty()) {
+            int curNode = q.poll();
+            count++;
+
+            for (int nei : graph.get(curNode)) {
+                if (!visited[nei]) {
+                    visited[nei] = true;
+                    q.offer(nei);
+                }
+            }
+        }
+
+        return count == n;
+    }
+}
