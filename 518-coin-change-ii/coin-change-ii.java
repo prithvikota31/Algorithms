0f11b417a5ceccdc309fd3d22b1d44e211d58c99
prithvikota31 <prithvikota@microsoft.com>
2026-07-03 17:51:34 -0700

    Time: 13 ms (41.91%) | Memory: 53.2 MB (59.45%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -8,60 +8,49 @@ class Solution {
         int m = dp.length;
         int n = dp[0].length;
 
+        int[] cur = new int[n];
+        int[] prev = new int[n];
+
+        // for(int j = 0; j < n; j++)
+        // {
+        //     if(j % coins[0] == 0)
+        //     {
+        //         dp[0][j] = 1;
+        //     }
+        // }
+
         for(int j = 0; j < n; j++)
         {
             if(j % coins[0] == 0)
             {
-                dp[0][j] = 1;
+                prev[j] = 1;
             }
         }
 
-        for(int i = 0; i < m; i++)
-        {
-            dp[i][0] = 1;
-        }
+        // for(int i = 0; i < m; i++)
+        // {
+        //     dp[i][0] = 1;
+        // }
 
         for(int i = 1; i < m; i++)
         {
+            cur[0] = 1;
             for(int j = 1; j < n; j++)
             {
                 int take = 0;
                 if(coins[i] <= j)
                 {
-                    take = dp[i][j - coins[i]];
+                    take = cur[j - coins[i]];
                 }
-                int notTake = dp[i - 1][j];
-                dp[i][j] = take + notTake;
+                int notTake = prev[j];
+                cur[j] = take + notTake;
             }
+
+            prev = cur.clone();
         }
 
-        return dp[m - 1][n - 1];
+        return prev[n - 1];
     }   
 
-    private int calculate(int index, int amount, int[] coins, int[][] dp)
-    {
-        if(index == 0)
-        {
-            if(amount % coins[index] == 0)
-            {
-                return 1;
-            }
-            else
-            {
-                return 0;   
-            }
-        }
 
-        if(dp[index][amount] != -1) return dp[index][amount];
-
-        //take
-        int take = 0;
-        if(amount >= coins[index])
-        {
-            take = calculate(index, amount - coins[index], coins, dp);
-        }
-        int notTake = calculate(index - 1, amount, coins, dp);
-
-        return dp[index][amount] = take + notTake;
-    }
 }
