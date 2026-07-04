3428f603ba905b6b386346d84fc64730f114d6e8
prithvikota31 <prithvikota@microsoft.com>
2026-07-04 11:23:30 -0700

    Time: 0 ms (100.00%) | Memory: 46.8 MB (19.55%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -1,21 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    private int maxSum = Integer.MIN_VALUE;
-
     public int maxPathSum(TreeNode root) {
-        dfs(root);
-        return maxSum;
+        int[] ans = new int[1];
+        ans[0] = Integer.MIN_VALUE;
+        getMaxPathSum(root, ans);
+        return ans[0];
+
     }
 
-    private int dfs(TreeNode root) {
-        if (root == null) return 0;
+    private int getMaxPathSum(TreeNode root, int[] max)
+    {
+        if(root == null)    return 0;
 
-        int left = Math.max(0, dfs(root.left));
-        int right = Math.max(0, dfs(root.right));
+        int left = Math.max(0, getMaxPathSum(root.left, max));
+        int right = Math.max(0, getMaxPathSum(root.right, max));
 
-        // best path using this node as the highest point
-        maxSum = Math.max(maxSum, root.val + left + right);
+        max[0] = Math.max(max[0], root.val + left + right);
 
-        // return best downward path
         return root.val + Math.max(left, right);
     }
 }
