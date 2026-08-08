bcba64bb72634a29d8612ff20dae18832498617d
prithvikota31 <prithvikota@microsoft.com>
2026-08-07 17:51:35 -0700

    Time: 2 ms (99.75%) | Memory: 45.6 MB (70.12%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,57 +1,62 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int[] freq = new int[128];
+        int tLen = t.length();
+        int sLen = s.length();
 
-        int sl = s.length();
-        int tl = t.length();
-
-        if(tl > sl) return "";
-
-        int checksNeeded = 0;
-        for(char ch: t.toCharArray())
-        {
-            freq[ch]++;
-            checksNeeded++;
-        }
-
-        //now sweep through s
-
-        int start = 0;
-        int minLength = Integer.MAX_VALUE;
-        int startIndex = 0;
-        for(int end = 0; end < sl; end++)
-        {
-            char ch = s.charAt(end);
-            if(freq[ch] > 0)
-            {
-                checksNeeded--;
-            }
-            freq[ch]--;
-
-            while(checksNeeded == 0)
-            {
-                if(end - start + 1 < minLength)
-                {
-                    minLength = end - start + 1;
-                    startIndex = start;
-                }
-
-                //try to reduce from forward
-                if(freq[s.charAt(start)] >= 0)
-                {
-                    checksNeeded++;
-                }
-
-                freq[s.charAt(start)]++;
-                start++;
-            }
-        }
-
-        if(minLength == Integer.MAX_VALUE)
+        if(tLen > sLen)
         {
             return "";
         }
 
-        return s.substring(startIndex, startIndex + minLength);
+        //freq array of t
+        int[] freq = new int[128];
+        int checksNeeded = 0;
+        for(int i = 0; i < tLen; i++)
+        {
+            freq[t.charAt(i)]++;
+            checksNeeded++;
+        }
+        //freq array contains values > 0 and =0 
+        //>0 corresponds to t chars freq
+
+        int start = 0;
+        int minLen = Integer.MAX_VALUE;;
+        int startIndex = 0;
+
+        for(int end = 0; end < sLen; end++)
+        {
+            int chEnd = s.charAt(end);
+            if(freq[chEnd] > 0)
+            {
+                checksNeeded--;
+            }
+            freq[chEnd]--;
+
+            while(checksNeeded == 0)
+            {
+                //capture this length
+                if(end - start + 1 < minLen)
+                {
+                    minLen = end - start + 1;
+                    startIndex = start;
+                }
+                //we can try to move start 
+                int chStart = s.charAt(start);
+                freq[chStart]++;
+                if(freq[chStart] > 0)
+                {
+                    checksNeeded++;
+                }
+                start++;
+            }
+        }
+        if(minLen == Integer.MAX_VALUE)
+        {
+            return "";
+        }
+
+        return s.substring(startIndex, startIndex + minLen);
+
+
     }
 }
