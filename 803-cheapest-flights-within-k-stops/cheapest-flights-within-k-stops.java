3e9661f0fcb77ed4c92fa98a8af7c5ea9fec030e
prithvikota31 <prithvikota@microsoft.com>
2026-08-25 00:21:08 -0700

    Time: 6 ms (85.48%) | Memory: 46.3 MB (83.42%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,62 +1,80 @@
+import java.util.*;
+
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        List<List<int[]>> graph = new ArrayList<>();
 
-        for(int i = 0; i < n; i++)
-        {
-            graph.add(new ArrayList<>());
+    public int findCheapestPrice(
+            int n,
+            int[][] flights,
+            int src,
+            int dst,
+            int k) {
+
+        List<List<int[]>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
         }
 
-        for(int[] flight: flights)
-        {
-            graph.get(flight[0]).add(new int[]{flight[1], flight[2]});
+        for (int[] flight : flights) {
+
+            int from = flight[0];
+            int to = flight[1];
+            int price = flight[2];
+
+            adj.get(from).add(
+                new int[]{to, price}
+            );
         }
 
-        //each graph node list has int[] of (dst, price)
-
-        Deque<int[]> q = new ArrayDeque<>();
-
-        q.offer(new int[]{0, src, 0}); //stops, node, totalpricesofar
-
-        //traverse through levels of stops like a bfs
         int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
 
-        Arrays.fill(dist, (int)1e9);
+        dist[src] = 0;
 
-        while(!q.isEmpty())
-        {
-            int[] current = q.poll();
-            int curStops = current[0];
-            int curCity = current[1];
-            int curPrice = current[2];
+        // {flightsUsed, node, cost}
+        Queue<int[]> queue = new ArrayDeque<>();
 
-            if(curStops > k)
-            {
-                break;
+        queue.offer(
+            new int[]{0, src, 0}
+        );
+
+        while (!queue.isEmpty()) {
+
+            int[] cur = queue.poll();
+
+            int flightsUsed = cur[0];
+            int node = cur[1];
+            int cost = cur[2];
+
+            // K stops = maximum K + 1 flights.
+            if (flightsUsed == k + 1) {
+                continue;
             }
 
-            for(int[] nei: graph.get(curCity))
-            {
-                int neiCity = nei[0];
-                int neiPrice = nei[1];
+            for (int[] edge : adj.get(node)) {
 
-                if(dist[neiCity] > neiPrice + curPrice && curStops <= k)
-                {
-                    dist[neiCity] = neiPrice + curPrice;
-                    q.offer(new int[]{curStops + 1, neiCity, dist[neiCity]});
+                int nei = edge[0];
+                int price = edge[1];
+
+                int newCost = cost + price;
+
+                if (newCost < dist[nei]) {
+
+                    dist[nei] = newCost;
+
+                    queue.offer(
+                        new int[]{
+                            flightsUsed + 1,
+                            nei,
+                            newCost
+                        }
+                    );
                 }
             }
         }
 
-        if(dist[dst] == (int)1e9)
-        {
-            return -1;
-        }
-        else
-        {
-            return dist[dst];
-        }
-
-
+        return dist[dst] == Integer.MAX_VALUE
+                ? -1
+                : dist[dst];
     }
 }
