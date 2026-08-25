c2edfe66e41233b2c983140044c22247282b14e6
prithvikota31 <prithvikota@microsoft.com>
2026-08-25 02:24:48 -0700

    Time: 8 ms (16.45%) | Memory: 48.3 MB (64.66%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.java
+++ 252-meeting-rooms/meeting-rooms.java
@@ -1,22 +1,22 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        if(intervals.length <= 1)   return true;
-        //minimum intervals length = 2
-        int[] lastMeeting = intervals[0];
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        //sorted based on end times
 
-        //check overlap for each meeting with previous one, if yes, return false
-        for(int i = 1; i < intervals.length; i++)
+        // if any adjacent interval overlap return false
+        // finally true
+
+        for(int i = 0; i <= intervals.length - 2; i++)
         {
-            if(lastMeeting[0] <= intervals[i][1] && 
-                lastMeeting[1] > intervals[i][0])
+            int[] interval1 = intervals[i];
+            int[] interval2 = intervals[i + 1];
+
+            //to overlap start < end 
+            if(interval1[0] < interval2[1] && interval2[0] < interval1[1])
             {
-                return false;     
+                return false;
             }
-            lastMeeting = intervals[i];
-
         }
-
         return true;
     }
 }
