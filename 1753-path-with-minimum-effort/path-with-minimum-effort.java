6419badd41c920059499bd3a37bc350ab10d6c63
prithvikota31 <prithvikota@microsoft.com>
2026-08-25 00:13:57 -0700

    Time: 42 ms (55.32%) | Memory: 48 MB (42.55%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,57 +1,53 @@
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-        
         int m = heights.length;
         int n = heights[0].length;
-        //sorted based on min effort travelled so far
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+
         int[][] effortGrid = new int[m][n];
 
-        //to move from (i, j) to next, it is abs(difference in heights)
-        effortGrid[0][0] = 0;
         for(int i = 0; i < m; i++)
         {
             Arrays.fill(effortGrid[i], (int)1e9);
         }
 
-        pq.offer(new int[]{0, 0, 0});
+        effortGrid[0][0] = 0;
+        //[effot, row, col]
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+        minHeap.offer(new int[]{0, 0, 0});
 
         int[] delRow = {0, 1, 0, -1};
         int[] delCol = {1, 0, -1, 0};
-        while(!pq.isEmpty())
+        while(!minHeap.isEmpty())
         {
-            int[] cur = pq.poll();
+            int[] cur = minHeap.poll();
             int cEffort = cur[0];
             int cRow = cur[1];
             int cCol = cur[2];
 
-            if(cEffort > effortGrid[cRow][cCol])
-            {
-                continue;
-            }
-
-            if(cRow == m -1 && cCol == n - 1)
+            if(cRow == m - 1 && cCol == n - 1)
             {
                 return cEffort;
             }
 
-            for(int i = 0; i < 4; i++)
+            for(int i = 0; i < delRow.length; i++)
             {
                 int nRow = cRow + delRow[i];
                 int nCol = cCol + delCol[i];
 
                 if(nRow >= 0 && nRow < m && nCol >= 0 && nCol < n)
                 {
-                    int nEffort = Math.max(cEffort, Math.abs(heights[nRow][nCol] - heights[cRow][cCol]));
+
+                    int diffEffort = Math.abs(heights[nRow][nCol] - heights[cRow][cCol]);
+                    int nEffort = Math.max(diffEffort, cEffort);
                     if(nEffort < effortGrid[nRow][nCol])
                     {
                         effortGrid[nRow][nCol] = nEffort;
-                        pq.offer(new int[]{nEffort, nRow, nCol});
-                    }
+                        minHeap.offer(new int[]{nEffort, nRow, nCol});
+                    }       
                 }
             }
         }
 
-        return effortGrid[m - 1][n - 1];
+        return 0;
     }
 }
