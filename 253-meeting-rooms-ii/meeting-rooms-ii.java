7551ef1ef78c5e7a4dbf9cd7848437e2411bce88
prithvikota31 <prithvikota@microsoft.com>
2026-08-25 02:34:35 -0700

    Time: 9 ms (51.95%) | Memory: 48.4 MB (72.22%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,37 +1,36 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        if (intervals.length == 0) return 0;
-
         int n = intervals.length;
-        int[] starts = new int[n];
-        int[] ends = new int[n];
 
-        // Split start times and end times.
-        for (int i = 0; i < n; i++) {
-            starts[i] = intervals[i][0];
-            ends[i] = intervals[i][1];
+        int[] start = new int[n];
+        int[] end = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
         }
 
-        // Sort both timelines independently.
-        Arrays.sort(starts);
-        Arrays.sort(ends);
-
-        int rooms = 0;
-        int endPtr = 0;
- 
-        // Process meetings by start time.
-        for (int startPtr = 0; startPtr < n; startPtr++) {
-
-            // Earliest meeting ended, so reuse that room.
-            if (starts[startPtr] >= ends[endPtr]) {
-                endPtr++;
-            } 
-            // No room free, need a new room.
-            else {
-                rooms++;
+        Arrays.sort(start);
+        Arrays.sort(end);
+        int usingRooms = 0;
+        int s = 0;
+        int e = 0;
+        int max = 0;
+        while(s < n && e < n)
+        {
+            if(start[s] < end[e])
+            {
+                usingRooms++;
+                s++;
             }
-        }
+            else
+            {
+                usingRooms--;
+                e++;
+            }
+            max = Math.max(max, usingRooms);
 
-        return rooms;
+        }
+        return max;
     }
 }
