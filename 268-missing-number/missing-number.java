e20b118b2500bbe148b92ff556189e195a12aa83
prithvikota31 <prithvikota@microsoft.com>
2026-08-26 17:43:28 -0700

    Time: 0 ms (100.00%) | Memory: 47.2 MB (82.37%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,13 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        int result = n;
-
+        int xor = 0;
         for(int i = 0; i < n; i++)
         {
-            result = result ^ (i ^ nums[i]);
+            xor = xor ^ i ^ nums[i];
         }
 
-        return result;
+        return xor ^ n;
+
     }
 }
