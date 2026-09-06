0545d090064fb64e2e802f892642ce309f22701b
prithvikota31 <prithvikota@microsoft.com>
2026-09-06 00:34:08 -0700

    Time: 1 ms (86.77%) | Memory: 44 MB (87.90%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,23 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        getSubsets(ans, 0, nums, new ArrayList<>());
+        List<Integer> temp = new ArrayList<>();
+        gatherSubsets(nums, 0, temp, ans);
         return ans;
     }
 
-    public void getSubsets(List<List<Integer>> ans, int index, int[] nums, List<Integer> trackingList)
+    private void gatherSubsets(int[] nums, int index, List<Integer> temp, List<List<Integer>> ans)
     {
-        if(index == nums.length)
+        ans.add(new ArrayList<>(temp));
+
+        for(int i = index; i < nums.length; i++)
         {
-            ans.add(new ArrayList<>(trackingList));
-            return;
+            temp.add(nums[i]);
+            gatherSubsets(nums, i + 1, temp, ans);
+            temp.remove(temp.size() - 1);
         }
-        trackingList.add(nums[index]);
-        getSubsets(ans, index + 1, nums, trackingList);
-        trackingList.remove(trackingList.size() - 1);
-        getSubsets(ans, index + 1, nums, trackingList);
-
     }
-
-
 }
