dbe76df7523b2691acee2ce678fdd0189dc1dba7
prithvikota31 <prithvikota@microsoft.com>
2026-09-05 23:16:45 -0700

    Time: 1 ms (100.00%) | Memory: 48.8 MB (51.74%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,38 +1,25 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
+        int m = nums1.length;
+        int n = nums2.length;
 
-        if(n1 > n2)
+        if(m > n)
         {
             return findMedianSortedArrays(nums2, nums1);
         }
+        int low = 0; //means 0 elements from nums1
+        int high = m; //all elements from nums1;
+        int left = (m + n + 1) / 2;
 
-        int leftCount = (n1 + n2 + 1) / 2;
-        //divide two arrays into left and right
-        // left elemetns can range from 0 to all
-        int left = 0;
-        int right = n1;
-
-        while(left <= right)
+        while(low <= high)
         {
-            int mid1 = right + (left - right) / 2; // 0 to mid1 - 1
-            int mid2 = leftCount - mid1; // o t0 mid2 - 1;
+            int mid1 = low + (high - low) / 2; //if mid1 = 3, nums1 contribute 3 elements
+            int mid2 = left - mid1; // right half started from mid, left half do not include md
 
-            //l1, l2, r1, r2
             int l1 = Integer.MIN_VALUE;
             int l2 = Integer.MIN_VALUE;
             int r1 = Integer.MAX_VALUE;
             int r2 = Integer.MAX_VALUE;
-
-            if(mid1 < n1)
-            {
-                r1 = nums1[mid1];
-            }
-            if(mid2 < n2)
-            {
-                r2 = nums2[mid2];
-            }
 
             if(mid1 - 1 >= 0)
             {
@@ -43,13 +30,24 @@ class Solution {
                 l2 = nums2[mid2 - 1];
             }
 
-            //by default l1 <= r1 and l2 <= r2
-            if(l1 <= r2 && l2 <= r1) //found answer
+            if(mid1 < m)
             {
-                int totalElements = n1 + n2;
-                if(totalElements % 2 == 0)
+                r1 = nums1[mid1];
+            }
+            if(mid2 < n)
+            {
+                r2 = nums2[mid2];
+            }
+
+            if(l1 <= r2 && l2 <= r1)
+            {
+                //odd or even elements
+                if((m + n) % 2 == 0)
                 {
-                    return ((double)Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                    int m1 = Math.max(l1, l2);
+                    int m2 = Math.min(r1, r2);
+
+                    return (double)(m1 + m2) / 2.0;
                 }
                 else
                 {
@@ -58,16 +56,14 @@ class Solution {
             }
             else if(l1 > r2)
             {
-                right = mid1 - 1;
+                high = mid1 - 1;
             }
             else
             {
-                left = mid1 + 1;
+                low = mid1 + 1;
             }
-
         }
-
-        return 0;
+        return -1;
 
     }
 }
