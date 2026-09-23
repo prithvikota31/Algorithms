cd3db1d7028a351b256bcba84acfeec449029054
prithvikota31 <prithvikota@microsoft.com>
2026-09-22 17:32:10 -0700

    Time: 7 ms (97.60%) | Memory: 48.1 MB (39.07%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -1,20 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 public class Codec {
 
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
+        //, for separator, # for null
         StringBuilder sb = new StringBuilder();
         serializeDfs(root, sb);
         return sb.toString();
     }
 
-    private void serializeDfs(TreeNode node, StringBuilder sb) {
-        if (node == null) {
+    //use postorder, and same order while deserialization
+    private void serializeDfs(TreeNode node, StringBuilder sb)
+    {
+        if(node == null)
+        {
             sb.append("#,");
             return;
         }
 
-        // Preorder: root -> left -> right
-        sb.append(node.val).append(",");
+        int val = node.val;
+        sb.append(val).append(",");
 
         serializeDfs(node.left, sb);
         serializeDfs(node.right, sb);
@@ -22,25 +35,24 @@ public class Codec {
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        String[] values = data.split(",");
+        String[] dataArray = data.split(",");
         int[] index = {0};
-
-        return deserializeDfs(values, index);
+        return deserializeDfs(dataArray, index);
     }
 
-    private TreeNode deserializeDfs(String[] values, int[] index) {
-        String val = values[index[0]++];
+    private TreeNode deserializeDfs(String[] dataArray, int[] index)
+    {
+        String val = dataArray[index[0]];
+        index[0]++;
 
-        if (val.equals("#")) {
+        if(val.equals("#"))
+        {
             return null;
         }
 
         TreeNode node = new TreeNode(Integer.parseInt(val));
-
-        // Same preorder order used during serialization
-        node.left = deserializeDfs(values, index);
-        node.right = deserializeDfs(values, index);
-
+        node.left = deserializeDfs(dataArray, index);
+        node.right = deserializeDfs(dataArray, index);
         return node;
     }
 }
