b3253b0581f260c853269483cffffe8abad465ab
prithvikota31 <prithvikota@microsoft.com>
2026-09-22 22:21:46 -0700

    Time: 1 ms (82.38%) | Memory: 43 MB (77.64%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,30 +1,33 @@
 class Solution {
     public int numDecodings(String s) {
-
         int n = s.length();
 
         int[] dp = new int[n + 1];
 
         dp[0] = 1;
 
-        for(int i = 1; i <= n; i++) {
-
-            // single digit
-            if(s.charAt(i - 1) != '0') {
+        //dp[i] maps to string index ending at i - 1
+        //that is first i characters
+        for(int i = 1; i <= n; i++)
+        {
+            //i as single digit
+            if(s.charAt(i - 1) != '0')
+            {
                 dp[i] += dp[i - 1];
             }
 
-            // two digits
-            if(i >= 2) {
-
-                int num =
-                    (s.charAt(i - 2) - '0') * 10
-                    + (s.charAt(i - 1) - '0');
-
-                if(num >= 10 && num <= 26) {
+            //ending at i - 1 as double digit
+            if(i >= 2)
+            {
+                int doubleDigit = (s.charAt(i - 2) - '0') * 10 
+                                        + (s.charAt(i - 1) - '0');
+                if(doubleDigit >= 10 && doubleDigit <= 26)
+                {
                     dp[i] += dp[i - 2];
                 }
+
             }
+
         }
 
         return dp[n];
