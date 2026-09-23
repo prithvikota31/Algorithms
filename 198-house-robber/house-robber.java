76a4e0f5181290f841085b1a97b126dd9f85df83
prithvikota31 <prithvikota@microsoft.com>
2026-09-22 21:05:01 -0700

    Time: 0 ms (100.00%) | Memory: 42.6 MB (87.54%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
-        //f(n) = max(f(n - 2) + nums[i], f(n - 1));
-        int n = nums.length;
-        int[] dp = new int[n + 1];
-
-        dp[0] = 0;
-        dp[1] = nums[0];
-
-        for(int i = 2; i <= n; i++)
+        //dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i])
+        //it represents max amount if we are allowed to rob till ith house
+        if(nums.length == 1)
         {
-            dp[i] = Math.max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+            return nums[0];
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < n; i++)
+        {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
         }
 
-        return dp[n];
-
+        return dp[n - 1];
     }
 }
