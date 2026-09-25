ba6ed48854d15328447b4adb97c470f0f3c3a50e
prithvikota31 <prithvikota@microsoft.com>
2026-09-24 21:03:03 -0700

    Time: 4 ms (80.50%) | Memory: 46.9 MB (46.61%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,67 +1,29 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) {
-            return null;
-        }
+        PriorityQueue<ListNode> minHeap =
+            new PriorityQueue<>((a, b) -> a.val - b.val);
 
-        return divide(lists, 0, lists.length - 1);
-    }
-
-    private ListNode divide(ListNode[] lists, int left, int right) //divide lists
-    {
-        if(left == right)
-        {
-            return lists[left];
-        }
-
-        int mid = left + (right - left) / 2;
-
-        ListNode leftList = divide(lists, left, mid);
-        ListNode rightList = divide(lists, mid + 1, right);
-
-        return mergeTwoLists(leftList, rightList);
-    }
-
-
-    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode finalNode = new ListNode();
-        ListNode dummy = finalNode;
-
-        while(list1 != null && list2 != null)
-        {
-            if(list1.val < list2.val)
-            {
-                dummy.next = list1;
-                list1 = list1.next;
+        // Put first node of every list into heap
+        for (ListNode node : lists) {
+            if (node != null) {
+                minHeap.offer(node);
             }
-            else
-            {
-                dummy.next = list2;
-                list2 = list2.next;
+        }
+
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+
+        while (!minHeap.isEmpty()) {
+            ListNode node = minHeap.poll();
+
+            cur.next = node;
+            cur = cur.next;
+
+            if (node.next != null) {
+                minHeap.offer(node.next);
             }
-
-            dummy = dummy.next;
         }
 
-        if(list1 == null)
-        {
-            dummy.next = list2;
-        }
-        else
-        {
-            dummy.next = list1;
-        }
-
-        return finalNode.next;
+        return dummy.next;
     }
 }
