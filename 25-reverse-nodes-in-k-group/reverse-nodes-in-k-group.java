f2d654df58190119be842f5d4b7843c0ee865c42
prithvikota31 <prithvikota@microsoft.com>
2026-09-24 21:53:11 -0700

    Time: 0 ms (100.00%) | Memory: 46.2 MB (74.06%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -6,55 +6,59 @@ class Solution {
         ListNode prevKEnd = dummy;
         ListNode oldHead = head;
 
-        while (oldHead != null) {
+        ListNode kth = getKthNode(oldHead, k);
 
-            ListNode kth = getKthNode(oldHead, k);
-
-            // fewer than k nodes left -> leave them unchanged
-            if (kth == null) {
-                prevKEnd.next = oldHead;
-                break;
-            }
-
+        while(kth != null)
+        {
             ListNode nextKStart = kth.next;
 
-            // cut current group
+            //cut current group
             kth.next = null;
 
-            // reverse current group
+            //reverse current group
             ListNode newHead = reverse(oldHead);
 
-            // reconnect
+            //connect previous group -> current reversed group
             prevKEnd.next = newHead;
+
+            //connect current reversed group -> next group
             oldHead.next = nextKStart;
 
-            // move to next group
+            //move pointers for next group
             prevKEnd = oldHead;
             oldHead = nextKStart;
+
+            kth = getKthNode(oldHead, k);
         }
 
         return dummy.next;
     }
 
-    private ListNode reverse(ListNode node) {
+    private ListNode reverse(ListNode node)
+    {
         ListNode prev = null;
         ListNode cur = node;
 
-        while (cur != null) {
-            ListNode next = cur.next;
+        while(cur != null)
+        {
+            ListNode curNext = cur.next;
             cur.next = prev;
             prev = cur;
-            cur = next;
+            cur = curNext;
         }
 
         return prev;
     }
 
-    private ListNode getKthNode(ListNode node, int k) {
-        for (int i = 1; i < k; i++) {
-            if (node == null) {
+    private ListNode getKthNode(ListNode node, int k)
+    {
+        for(int i = 1; i < k; i++)
+        {
+            if(node == null)
+            {
                 return null;
             }
+
             node = node.next;
         }
 
